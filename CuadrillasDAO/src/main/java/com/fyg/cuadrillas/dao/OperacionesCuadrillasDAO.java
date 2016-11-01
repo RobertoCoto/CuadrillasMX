cd43042b7560be13222b24d231b14fdb2364f6e7
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-01 12:19:31 -0600

    se añade cambios

--- CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/OperacionesCuadrillasDAO.java
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/OperacionesCuadrillasDAO.java
@@ -8,36 +8,36 @@ import com.fyg.cuadrillas.comun.LogHandler;
 import com.fyg.cuadrillas.dao.resources.FabricaConexiones;
 import com.fyg.cuadrillas.dto.PruebaDTO;
 
-public class OperacionesCruadrillasDAO {
+public class OperacionesCuadrillasDAO {
 	
 	
  public EncabezadoRespuesta registraPrueba(String uid, PruebaDTO prueba) {
-	 	SqlSession sessionTx = null;
+	 	SqlSession sessionNTx = null;
 		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
 		respuesta.setUid(uid);
 		respuesta.setEstatus(true);
 		respuesta.setMensajeFuncional("Registro correcto.");
 		try {
 			//Abrimos conexion Transaccional
-			sessionTx = FabricaConexiones.obtenerSesionTx();
-     	int registros = sessionTx.insert("OperacionesCuadrillasDAO.insertaPrueba", prueba);
+			sessionNTx = FabricaConexiones.obtenerSesionTx();
+     	int registros = sessionNTx.insert("OperacionesCuadrillasDAO.insertaPrueba", prueba);
 			if ( registros == 0) {
 				throw new ExcepcionesCuadrillas("Error en registrar.");
 			}
 			//Realizamos commit
 			LogHandler.debug(uid, this.getClass(), "Commit!!!");
-			sessionTx.commit();
+			sessionNTx.commit();
 		}
 		catch (Exception ex) {
 			//Realizamos rollBack
 			LogHandler.debug(uid, this.getClass(), "RollBack!!!");
-			FabricaConexiones.rollBack(sessionTx);
+			FabricaConexiones.rollBack(sessionNTx);
          LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
          respuesta.setEstatus(false);
  		respuesta.setMensajeFuncional(ex.getMessage());
 		}
 		finally {
-			FabricaConexiones.close(sessionTx);
+			FabricaConexiones.close(sessionNTx);
 		}
 		return respuesta;
  }
--- CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/TestConexion.java
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/TestConexion.java
@@ -1,14 +1,42 @@
 package com.fyg.cuadrillas.dao;
 
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
+import com.fyg.cuadrillas.dto.PruebaDTO;
+import com.fyg.cuadrillas.dao.OperacionesCuadrillasDAO;
+import com.fyg.cuadrillas.comun.GUIDGenerator;
+import com.fyg.cuadrillas.comun.LogHandler;
 public class TestConexion {
 
+	private OperacionesCuadrillasDAO datoPrueba;
+	private PruebaDTO pruebas;
+	private GUIDGenerator uid;
+	@Before
+	public void setUp() throws Exception {
+		 datoPrueba = new OperacionesCuadrillasDAO();
+		 
+		 /**
+		  * se le envian los datos a actividad
+		  */
+		pruebas = new PruebaDTO();
+		pruebas.setUsuario("HUSKY");
+		pruebas.setContraseña("123456");
+		pruebas.setMensaje("mensaje de apoyo shabo");
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@SuppressWarnings("static-access")
+	public void testRegistraActividad() throws Exception {
+		
+		String guid = uid.generateGUID(datoPrueba);
+		try {
+			datoPrueba.registraPrueba(guid, pruebas);
+			LogHandler.debug(guid,this.getClass(), "PRUEBA JUNIT");
+		}
+		catch (Exception ex) {
+			LogHandler.debug(guid,this.getClass(), "Error");
+		}
 	}
 
 }
