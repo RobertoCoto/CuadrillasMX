333a7bb5c97ecabfca42e0a848cbb53ae2861392
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-04 11:22:38 -0600

    cambios 1

--- CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/OperacionesCuadrillasDAO.java
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/OperacionesCuadrillasDAO.java
@@ -6,6 +6,7 @@ import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
 import com.fyg.cuadrillas.comun.ExcepcionesCuadrillas;
 import com.fyg.cuadrillas.comun.LogHandler;
 import com.fyg.cuadrillas.dao.resources.FabricaConexiones;
+import com.fyg.cuadrillas.dto.Catalogos;
 import com.fyg.cuadrillas.dto.PruebaDTO;
 import com.fyg.cuadrillas.dto.Usuario;
 
@@ -111,6 +112,78 @@ public class OperacionesCuadrillasDAO {
 			FabricaConexiones.rollBack(sessionTx);
       LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
       respuesta.setEstatus(false);
+		respuesta.setMensajeFuncional(ex.getMessage());
+		}
+		finally {
+			FabricaConexiones.close(sessionTx);
+		}
+		return respuesta;
+}
+ /**
+  * Metodo para la baja de un catalogo
+  * @param uid unico de registro
+  * @param catalogoOV recibe los valores del catalogo
+  * @return regresa el resultado de la baja
+  */
+ public EncabezadoRespuesta eliminarCatalogo(String uid, Catalogos catalogoOV) {
+	 	SqlSession sessionTx = null;
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		respuesta.setUid(uid);
+		respuesta.setEstatus(true);
+		respuesta.setMensajeFuncional("Baja correcta.");
+		try {
+			//Abrimos conexion Transaccional
+			sessionTx = FabricaConexiones.obtenerSesionTx();
+	        int registros = sessionTx.update("OperacionesCuadrillasDAO.inactivaTipoCatalogo", catalogoOV);
+			if ( registros == 0) {
+				throw new ExcepcionesCuadrillas("Error al bajar el catalogo.");
+			}
+			//Realizamos commit
+			LogHandler.debug(uid, this.getClass(), "Commit!!!");
+			sessionTx.commit();
+		}
+		catch (Exception ex) {
+			//Realizamos rollBack
+			LogHandler.debug(uid, this.getClass(), "RollBack!!!");
+			FabricaConexiones.rollBack(sessionTx);
+   LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
+   respuesta.setEstatus(false);
+		respuesta.setMensajeFuncional(ex.getMessage());
+		}
+		finally {
+			FabricaConexiones.close(sessionTx);
+		}
+		return respuesta;
+}
+ /**
+  * Metodo para registrar Catalogos
+  * @param uid unico de registro
+  * @param catalogoOV recibe valores de catalogo
+  * @return regresa si el registro fue correcto
+  */
+ public EncabezadoRespuesta registraCatalogo(String uid, Catalogos catalogoOV) {
+	 	SqlSession sessionTx = null;
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		respuesta.setUid(uid);
+		respuesta.setEstatus(true);
+		respuesta.setMensajeFuncional("registro correcto.");
+		try {
+			//Abrimos conexion Transaccional
+			sessionTx = FabricaConexiones.obtenerSesionTx();
+	        int registros = sessionTx.update("OperacionesCuadrillasDAO.registraCatalogo", catalogoOV);
+			if ( registros == 0) {
+				throw new ExcepcionesCuadrillas("Error al registrar el catalogo.");
+			}
+			//Realizamos commit
+			LogHandler.debug(uid, this.getClass(), "Commit!!!");
+			sessionTx.commit();
+		}
+		catch (Exception ex) {
+			//Realizamos rollBack
+			LogHandler.debug(uid, this.getClass(), "RollBack!!!");
+			FabricaConexiones.rollBack(sessionTx);
+LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
+respuesta.setEstatus(false);
 		respuesta.setMensajeFuncional(ex.getMessage());
 		}
 		finally {
