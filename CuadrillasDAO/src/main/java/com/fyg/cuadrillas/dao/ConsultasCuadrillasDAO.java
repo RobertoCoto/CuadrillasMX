715b2922abf877632279fe7e75482e2fc0da123b
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-07 12:27:05 -0600

    se hacen cambios

--- CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/ConsultasCuadrillasDAO.java
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/ConsultasCuadrillasDAO.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 
 import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
+import com.fyg.cuadrillas.comun.ExcepcionesCuadrillas;
 import com.fyg.cuadrillas.comun.LogHandler;
 import com.fyg.cuadrillas.dao.resources.FabricaConexiones;
-import com.fyg.cuadrillas.dto.Catalogos;
 import com.fyg.cuadrillas.dto.Herramientas;
 import com.fyg.cuadrillas.dto.Perfil;
 import com.fyg.cuadrillas.dto.PruebaDTO;
@@ -46,183 +46,39 @@ public class ConsultasCuadrillasDAO {
 		return listaPrueba;
 	}
 	/**
-	 * Metodo para consultar usuario
-	 * @param uid unico
-	 * @param usuario recibe valores de usuario
-	 * @return regresa resultado de usuarios
+	 * Metodo para registrar datos prueba
+	 * @param uid id unico de registro
+	 * @param prueba , recibe valores de prueba
+	 * @return regresa respuesta de registro
 	 */
-	@SuppressWarnings("unchecked")
-	public List<Usuario> consultaUsuario(String uid, Usuario usuario) {
-		SqlSession sessionNTx = null;
-		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
-		respuesta.setUid(uid);
-		respuesta.setEstatus(true);
-		respuesta.setMensajeFuncional("Consulta correcta.");
-		List<Usuario> listaUsuario = null;
-		try {
-			//Abrimos conexion Transaccional
-			sessionNTx = FabricaConexiones.obtenerSesionNTx();
-			//Se hace una consulta a la tabla contacto
-			listaUsuario = sessionNTx.selectList("ConsultasCuadrillasDAO.consultaUsuario", usuario);
-		}
-		catch (Exception ex) {
-			LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
-            respuesta.setEstatus(false);
-    		respuesta.setMensajeFuncional(ex.getMessage());
-		}
-		finally {
-			FabricaConexiones.close(sessionNTx);
-		}
-		return listaUsuario;
-	}
-	/**
-	 * Metodo para consultar el perfil existente
-	 * @param uid unico
-	 * @param perfil recibe valores perfil
-	 * @return regresa resultados
-	 */
-	@SuppressWarnings("unchecked")
-	public List<Perfil> consultaPerfil(String uid, Perfil perfil) {
-		SqlSession sessionNTx = null;
-		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
-		respuesta.setUid(uid);
-		respuesta.setEstatus(true);
-		respuesta.setMensajeFuncional("Consulta correcta.");
-		List<Perfil> listaPerfil = null;
-		try {
-			//Abrimos conexion Transaccional
-			sessionNTx = FabricaConexiones.obtenerSesionNTx();
-			//Se hace una consulta a la tabla contacto
-			listaPerfil = sessionNTx.selectList("ConsultasCuadrillasDAO.consultaPerfil", perfil);
-		}
-		catch (Exception ex) {
-			LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
-            respuesta.setEstatus(false);
-    		respuesta.setMensajeFuncional(ex.getMessage());
-		}
-		finally {
-			FabricaConexiones.close(sessionNTx);
-		}
-		return listaPerfil;
-	}
-	/**
-	 * Metodo para consultar los catalogos
-	 * @param uid unico de registro
-	 * @param catalogo valores de catalogos
-	 * @return regresa una lista de catalogos
-	 */
-	@SuppressWarnings("unchecked")
-	public List<Catalogos> consultaCatalogo(String uid, Catalogos catalogo) {
-		SqlSession sessionNTx = null;
-		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
-		respuesta.setUid(uid);
-		respuesta.setEstatus(true);
-		respuesta.setMensajeFuncional("Consulta correcta.");
-		List<Catalogos> listaCatalogos = null;
-		try {
-			//Abrimos conexion Transaccional
-			sessionNTx = FabricaConexiones.obtenerSesionNTx();
-			//Se hace una consulta a la tabla contacto
-			listaCatalogos = sessionNTx.selectList("ConsultasCuadrillasDAO.consultaCatalogo", catalogo);
-		}
-		catch (Exception ex) {
-			LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
-            respuesta.setEstatus(false);
-    		respuesta.setMensajeFuncional(ex.getMessage());
-		}
-		finally {
-			FabricaConexiones.close(sessionNTx);
-		}
-		return listaCatalogos;
-	}
-	/**
-	 * Metodo para consultar los catalogos
-	 * @param uid unico de registro
-	 * @param catalogoOV valores de catalogos
-	 * @return regresa una lista de catalogos
-	 */
-	@SuppressWarnings("unchecked")
-	public List<Catalogos> consultaListaCatalogo(String uid, Catalogos catalogoOV) {
-		SqlSession sessionNTx = null;
-		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
-		respuesta.setUid(uid);
-		respuesta.setEstatus(true);
-		respuesta.setMensajeFuncional("Consulta correcta.");
-		List<Catalogos> listaCatalogos = null;
-		try {
-			//Abrimos conexion Transaccional
-			sessionNTx = FabricaConexiones.obtenerSesionNTx();
-			//Se hace una consulta a la tabla contacto
-			listaCatalogos = sessionNTx.selectList("ConsultasCuadrillasDAO.consultaListaCatalogo", catalogoOV);
-		}
-		catch (Exception ex) {
-			LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
-            respuesta.setEstatus(false);
-    		respuesta.setMensajeFuncional(ex.getMessage());
-		}
-		finally {
-			FabricaConexiones.close(sessionNTx);
-		}
-		return listaCatalogos;
-	}
-	/**
-	 * Metodo para consultar Herramientas
-	 * @param uid unico de registro
-	 * @param herramientasOV recibe valores de herramienta
-	 * @return regresa lista de herramientas
-	 */
-	@SuppressWarnings("unchecked")
-	public List<Herramientas> consultaHerramientas(String uid, Herramientas herramientasOV) {
-		SqlSession sessionNTx = null;
-		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
-		respuesta.setUid(uid);
-		respuesta.setEstatus(true);
-		respuesta.setMensajeFuncional("Consulta correcta.");
-		List<Herramientas> listaHerramientas = null;
-		try {
-			//Abrimos conexion Transaccional
-			sessionNTx = FabricaConexiones.obtenerSesionNTx();
-			//Se hace una consulta a la tabla contacto
-			listaHerramientas = sessionNTx.selectList("ConsultasCuadrillasDAO.consultaHerramientas", herramientasOV);
-		}
-		catch (Exception ex) {
-			LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
-            respuesta.setEstatus(false);
-    		respuesta.setMensajeFuncional(ex.getMessage());
-		}
-		finally {
-			FabricaConexiones.close(sessionNTx);
-		}
-		return listaHerramientas;
-	}
-	/**
-	 * Metodo general para consultar
-	 * @param uid unico de registro
-	 * @param herramientasOV recibe valores de herramientas
-	 * @return regresa una lista de herramientas
-	 */
-	@SuppressWarnings("unchecked")
-	public List<Herramientas> consultaListaHerramientas(String uid, Herramientas herramientasOV) {
-		SqlSession sessionNTx = null;
-		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
-		respuesta.setUid(uid);
-		respuesta.setEstatus(true);
-		respuesta.setMensajeFuncional("Consulta correcta.");
-		List<Herramientas> listaHerramientas = null;
-		try {
-			//Abrimos conexion Transaccional
-			sessionNTx = FabricaConexiones.obtenerSesionNTx();
-			//Se hace una consulta a la tabla contacto
-			listaHerramientas = sessionNTx.selectList("ConsultasCuadrillasDAO.consultaListaHerramientas", herramientasOV);
-		}
-		catch (Exception ex) {
-			LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
-            respuesta.setEstatus(false);
-    		respuesta.setMensajeFuncional(ex.getMessage());
-		}
-		finally {
-			FabricaConexiones.close(sessionNTx);
-		}
-		return listaHerramientas;
-	}
+	 public EncabezadoRespuesta registraPrueba(String uid, PruebaDTO prueba) {
+		 	SqlSession sessionTx = null;
+			EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+			respuesta.setUid(uid);
+			respuesta.setEstatus(true);
+			respuesta.setMensajeFuncional("Registro correcto.");
+			try {
+				//Abrimos conexion Transaccional
+				sessionTx = FabricaConexiones.obtenerSesionTx();
+	     	int registros = sessionTx.insert("OperacionesCuadrillasDAO.insertaPrueba", prueba);
+				if ( registros == 0) {
+					throw new ExcepcionesCuadrillas("Error en registrar.");
+				}
+				//Realizamos commit
+				LogHandler.debug(uid, this.getClass(), "Commit!!!");
+				sessionTx.commit();
+			}
+			catch (Exception ex) {
+				//Realizamos rollBack
+				LogHandler.debug(uid, this.getClass(), "RollBack!!!");
+				FabricaConexiones.rollBack(sessionTx);
+	         LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
+	         respuesta.setEstatus(false);
+	 		respuesta.setMensajeFuncional(ex.getMessage());
+			}
+			finally {
+				FabricaConexiones.close(sessionTx);
+			}
+			return respuesta;
+	 }
 }
