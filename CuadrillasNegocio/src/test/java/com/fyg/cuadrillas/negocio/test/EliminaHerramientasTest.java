c2214e2a15a958b6b344a9e0ea95acad57c444cd
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-07 13:54:03 -0600

    se reconfigura test

---
+++ CuadrillasNegocio/src/test/java/com/fyg/cuadrillas/negocio/test/EliminaHerramientasTest.java
@@ -0,0 +1,51 @@
+package com.fyg.cuadrillas.negocio.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fyg.cuadrillas.negocio.HerramientasNegocio;
+import com.fyg.cuadrillas.comun.GUIDGenerator;
+import com.fyg.cuadrillas.comun.LogHandler;
+import com.fyg.cuadrillas.dto.Herramientas;
+
+public class EliminaHerramientasTest {
+	/**
+	* objeto para enviar al metodo consulta de catalogo
+	*/
+	private HerramientasNegocio eliminaHerramienta;
+	/**
+	 * Recibe datos de herramientas
+	 */
+	private Herramientas datoHerramientas;
+	/**
+	 * Guid unico generado
+	 */
+	private GUIDGenerator uid;
+	/**
+	 * SetUp
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		eliminaHerramienta = new HerramientasNegocio();
+		datoHerramientas = new Herramientas();
+		datoHerramientas.setOrden("D");
+		datoHerramientas.setEstatus("A");
+	}
+	/**
+	 * Metodo test que le envia datos
+	 * @throws Exception
+	 */
+	@SuppressWarnings("static-access")
+	@Test
+	public void testEliminaHerramientas() throws Exception {
+		String guid = uid.generateGUID(eliminaHerramienta);
+		try {
+			eliminaHerramienta.consultarHerramientas(datoHerramientas);
+		}
+		catch (Exception ex) {
+			LogHandler.debug(guid, this.getClass(), "Error");
+		}
+	}
+
+}
