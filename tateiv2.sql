8ce115b2e0823f447d8de94718d15f26bf957cbf
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-08 16:39:27 -0600

    se actualiza querys

--- tateiv2.sql
+++ tateiv2.sql
@@ -177,3 +177,20 @@ VALUES (2,"Oficina Central","Oficina Central",'A');
 INSERT INTO perfil (id_perfil,nombre,descripcion,estatus) 
 VALUES (3,"Recursos Humanos","Recursos Humanos",'A');
 
+INSERT INTO menu (id_menu,menu,descripcion,estatus)
+VALUES (1,"Menu Padre","Menu Padre",'A');
+
+INSERT INTO menu (id_menu,menu,descripcion,url,estatus)
+VALUES  (2,1,"Menu Hijo 1","Menu Hijo 1","/Hijo.html",'A');
+
+INSERT INTO menu (id_menu,menu,descripcion,url,estatus)
+VALUES  (3,1,"Menu Hijo 2","Menu Hijo 2","/Hijo2.html",'A');
+
+INSERT INTO perfil_menu (id_perfil,id_menu,orden,estatus)
+VALUES (0,1,10,'A');
+
+INSERT INTO perfil_menu (id_perfil,id_menu,orden,estatus)
+VALUES (0,2,20,'A');
+
+INSERT INTO perfil_menu (id_perfil,id_menu,orden,estatus)
+VALUES (0,3,30,'A');
