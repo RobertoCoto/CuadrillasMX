61329ff89a7d799a35678dcabd69e912ecd9d40d
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-09 11:29:15 -0600

    prebuild

--- CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/JSONServiceUsuario.java
+++ CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/JSONServiceUsuario.java
@@ -1,5 +1,24 @@
 package com.fyg.cuadrillas.web;
+import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.fyg.cuadrillas.negocio.UsuariosNegocio;
+
+@Path("/userLogin")
 public class JSONServiceUsuario {
-
+	
+	  @Path("{i}")
+	  @GET
+	  @Produces("application/json")
+	  public Response jsonUsuario(@PathParam("i") Integer f) throws JSONException {
+		    JSONObject jsonObject = new JSONObject();
+		    String result = "" + jsonObject;
+			return Response.status(200).entity(result).build();
+	  }
 }
