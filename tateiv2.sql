262f82a023c7e7fedd8105956a57175c98589318
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-09 16:59:53 -0600

    se corrije query para sacar menu

--- tateiv2.sql
+++ tateiv2.sql
@@ -169,21 +169,21 @@ INSERT INTO perfil (id_perfil,nombre,descripcion,estatus)
 VALUES (0,"Sistemas","Sistemas",'A');
 
 INSERT INTO perfil (id_perfil,nombre,descripcion,estatus) 
-VALUES (1,"Residente","Residente",'A');
+VALUES (2,"Residente","Residente",'A');
 
 INSERT INTO perfil (id_perfil,nombre,descripcion,estatus) 
-VALUES (2,"Oficina Central","Oficina Central",'A');
+VALUES (3,"Oficina Central","Oficina Central",'A');
 
 INSERT INTO perfil (id_perfil,nombre,descripcion,estatus) 
-VALUES (3,"Recursos Humanos","Recursos Humanos",'A');
+VALUES (4,"Recursos Humanos","Recursos Humanos",'A');
 
 INSERT INTO menu (id_menu,menu,descripcion,estatus)
 VALUES (1,"Menu Padre","Menu Padre",'A');
 
-INSERT INTO menu (id_menu,menu,descripcion,url,estatus)
+INSERT INTO menu (id_menu,id_padre,menu,descripcion,url,estatus)
 VALUES  (2,1,"Menu Hijo 1","Menu Hijo 1","/Hijo.html",'A');
 
-INSERT INTO menu (id_menu,menu,descripcion,url,estatus)
+INSERT INTO menu (id_menu,id_padre,menu,descripcion,url,estatus)
 VALUES  (3,1,"Menu Hijo 2","Menu Hijo 2","/Hijo2.html",'A');
 
 INSERT INTO perfil_menu (id_perfil,id_menu,orden,estatus)
