ddc0dadef576ccce07203da1ddd061c953f60daf
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-11 10:56:18 -0600

    Se agrega metodo reistraEmp en DAO

--- CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/EmpleadoDAO.java
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/EmpleadoDAO.java
@@ -1,5 +1,41 @@
 package com.fyg.cuadrillas.dao;
 
-public class EmpleadoDAO {
+import org.apache.ibatis.session.SqlSession;
 
+import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
+import com.fyg.cuadrillas.comun.ExcepcionesCuadrillas;
+import com.fyg.cuadrillas.comun.LogHandler;
+import com.fyg.cuadrillas.dto.Empleado;
+
+public class EmpleadoDAO {
+	 public EncabezadoRespuesta registraEmpleado(String uid,Empleado empleado) {
+		 	SqlSession sessionTx = null;
+			EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+			respuesta.setUid(uid);
+			respuesta.setEstatus(true);
+			respuesta.setMensajeFuncional("registro correcto.");
+			try {
+				//Abrimos conexion Transaccional
+				sessionTx = FabricaConexiones.obtenerSesionTx();
+		        int registros = sessionTx.update("EmpleadoDAO.registraEmpleado", empleado);
+				if ( registros == 0) {
+					throw new ExcepcionesCuadrillas("Error al registrar el empleado.");
+				}
+				//Realizamos commit
+				LogHandler.debug(uid, this.getClass(), "Commit!!!");
+				sessionTx.commit();
+			}
+			catch (Exception ex) {
+				//Realizamos rollBack
+				LogHandler.debug(uid, this.getClass(), "RollBack!!");
+				FabricaConexiones.rollBack(sessionTx);
+				LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
+				respuesta.setEstatus(false);
+				respuesta.setMensajeFuncional(ex.getMessage());
+			}
+			finally {
+				FabricaConexiones.close(sessionTx);
+			}
+			return respuesta;
+	}
 }
