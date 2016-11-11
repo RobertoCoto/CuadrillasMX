302fb29c091dcf0945cab69295f0a231ef58cb36
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-11 09:51:45 -0600

    Empleado

--- CuadrillasDTO/src/main/java/com/fyg/cuadrillas/dto/Empleado.java
+++ CuadrillasDTO/src/main/java/com/fyg/cuadrillas/dto/Empleado.java
@@ -1,5 +1,43 @@
 package com.fyg.cuadrillas.dto;
 
+import java.util.Date;
+
 public class Empleado {
+/**
+ * Id unico del empleado
+ */
+private Integer id_empleado;
+/**
+ * Nombre del empleado
+ */
+private String nombre;
+/**
+ * Apellido Paterno del usuario
+ */
+private String apellido_pat;
+/**
+ * Apellido Materno del usuario
+ */
+private String apellido_mat;
+/**
+ * Sexo del usuario
+ */
+private String sexo;
+/**
+ * RFC del usuario
+ */
+private String rfc;
+/**
+ * rfc_calculado del usuario
+ */
+private String rfc_calculado;
+/**
+ * Curp del usuario
+ */
+private String curp;
+/**
+ * Fecha_nacimiento del usuario
+ */
+private Date fecha_nacimiento;
 
 }
