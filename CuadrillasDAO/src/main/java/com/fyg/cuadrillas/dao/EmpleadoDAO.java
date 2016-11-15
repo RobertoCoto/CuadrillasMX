352f48aa3cb659c04ccb7222140916451cf07695
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-15 11:53:26 -0600

    metodo dao modEmpleado

--- CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/EmpleadoDAO.java
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/EmpleadoDAO.java
@@ -169,5 +169,43 @@ public List<Empleado> consultaEmpleado(String uid, Empleado empleado) {
 		}
 		return listaEmpleado;
 	}
+   /**
+    * Metodo para modificar los datos del empleado
+    * @param uid unico de registro
+    * @param empleado recibe valores de empleado
+    * @return regresa la respuesta
+    */
+   public EncabezadoRespuesta modificaEmpleado(String uid,Empleado empleado) {
+	 	SqlSession sessionTx = null;
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		respuesta.setUid(uid);
+		respuesta.setEstatus(true);
+		respuesta.setMensajeFuncional("baja correcto.");
+		try {
+			//Abrimos conexion Transaccional
+			sessionTx = FabricaConexiones.obtenerSesionTx();
+	        int registros = sessionTx.update("EmpleadoDAO.modificaEmpleado", empleado);
+			if ( registros == 0) {
+				throw new ExcepcionesCuadrillas("Error al modificar al empleado.");
+			}
+          
+			//Realizamos commit
+			LogHandler.debug(uid, this.getClass(), "Commit!!!");
+			sessionTx.commit();
+			
+		}
+		catch (Exception ex) {
+			//Realizamos rollBack
+			LogHandler.debug(uid, this.getClass(), "RollBack!!");
+			FabricaConexiones.rollBack(sessionTx);
+			LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
+			respuesta.setEstatus(false);
+			respuesta.setMensajeFuncional(ex.getMessage());
+		}
+		finally {
+			FabricaConexiones.close(sessionTx);
+		}
+		return respuesta;
+}
    }
 
