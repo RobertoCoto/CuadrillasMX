d048fcb1b4c5d15f7f85ad60b02fec37bf88705b
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-25 13:48:33 -0600

    correcion

--- CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/empleado/RegistraEmpleado.java
+++ CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/empleado/RegistraEmpleado.java
@@ -12,12 +12,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
+
 import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
 import com.fyg.cuadrillas.comun.LogHandler;
 import com.fyg.cuadrillas.dto.empleado.EmpleadoDTO;
 import com.fyg.cuadrillas.dto.empleado.EmpleadoDocumentoDTO;
 import com.fyg.cuadrillas.negocio.EmpleadoNegocio;
 import com.google.gson.Gson;
+import com.google.gson.JsonParser;
 /**
  * Servlet implementation class AltaEmpleado
  */
@@ -70,11 +73,12 @@ public class RegistraEmpleado extends HttpServlet {
 			String noCreditoInfonavit = request.getParameter("noCreditoInfonavit");
 			String observaciones = request.getParameter("observaciones");
 			String usuario = request.getParameter("usuario");
-		   String[]  codigoDocumento = request.getParameterValues("codigoDocumento");
-		   String[] estatusDocumento = request.getParameterValues("estatusDocumento");
 			
-		  
-		   
+			//se parsea json
+			JsonParser parser = new JsonParser();
+			Object documentoEmpleado = parser.parse(request.getParameter("documentoEmpleado"));
+			JSONObject jsonObject = (JSONObject) documentoEmpleado;
+
 			/* proxy fisa
 			System.setProperty("http.proxyHost", "169.169.4.85");
 	        System.setProperty("http.proxyPort", "8080");
@@ -114,14 +118,12 @@ public class RegistraEmpleado extends HttpServlet {
 			empleado.setUsuarioAlta(usuario);
 		    
 			//documentos
+			String codigoDocumento = (String) jsonObject.get("codigoDocumento");
+			String estatusDocumento = (String) jsonObject.get("estatusDocumento");
+		    
+			codigo.setCodigoEmpDoc(codigoDocumento);
+			codigo.setEstatus(estatusDocumento);
 			
-			for (int i=0; i< codigoDocumento.length;i++) {
-				codigo.setCodigoEmpDoc(codigoDocumento[i]);
-			}
-			
-			for(int k=0; k < estatusDocumento.length; k++) {
-				codigo.setEstatus(estatusDocumento[k]);
-			}	
 			documentos.add(codigo);
 			empleado.setDocumentos(documentos);
 			
