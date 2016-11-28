469d8bca7e28dd6c811a784392a0f6a4f58781f8
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-28 12:44:59 -0600

    Asistencia Respuesta

--- CuadrillasDTO/src/main/java/com/fyg/cuadrillas/dto/asistencia/AsistenciaRespuesta.java
+++ CuadrillasDTO/src/main/java/com/fyg/cuadrillas/dto/asistencia/AsistenciaRespuesta.java
@@ -1,5 +1,42 @@
 package com.fyg.cuadrillas.dto.asistencia;
 
-public class AsistenciaRespuesta {
+import java.util.List;
 
+import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
+import com.fyg.cuadrillas.comun.ObjetoValor;
+
+public class AsistenciaRespuesta extends ObjetoValor {
+
+	/**
+	 * Serial UID
+	 */
+	private static final long serialVersionUID = -679840902203050424L;
+	/** respuesta */
+	private EncabezadoRespuesta header;
+	/** Lista asistencia */
+	private List<AsistenciaDTO> asistencia;
+	/**
+	 * @return the header
+	 */
+	public EncabezadoRespuesta getHeader() {
+		return header;
+	}
+	/**
+	 * @param header the header to set
+	 */
+	public void setHeader(EncabezadoRespuesta header) {
+		this.header = header;
+	}
+	/**
+	 * @return the asistencia
+	 */
+	public List<AsistenciaDTO> getAsistencia() {
+		return asistencia;
+	}
+	/**
+	 * @param asistencia the asistencia to set
+	 */
+	public void setAsistencia(List<AsistenciaDTO> asistencia) {
+		this.asistencia = asistencia;
+	}
 }
