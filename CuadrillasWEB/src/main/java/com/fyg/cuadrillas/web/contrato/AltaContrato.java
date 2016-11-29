098a350ef9a83549d61647d06a7a68b27f347685
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-29 15:32:44 -0600

    servlet alta contrato

--- CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/contrato/AltaContrato.java
+++ CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/contrato/AltaContrato.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
 import com.fyg.cuadrillas.comun.LogHandler;
+import com.fyg.cuadrillas.dto.contrato.ContratoDTO;
+import com.fyg.cuadrillas.negocio.ContratoNegocio;
 import com.google.gson.Gson;
 
 /**
@@ -43,7 +45,31 @@ public class AltaContrato extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		
 		try {
+			Integer numeroContrato = Integer.parseInt(request.getParameter("numeroContrato"));
+			Integer idEmpleado = Integer.parseInt(request.getParameter("idEmpleado"));
+			Integer idVialidad = Integer.parseInt(request.getParameter("idVialidad"));
+			String tramoInicial = request.getParameter("tramoInicial");
+			String tramoFinal = request.getParameter("tramoFinal");
 			
+			/* descomentar para proxy FISA
+			System.setProperty("http.proxyHost", "169.169.4.85");
+	        System.setProperty("http.proxyPort", "8080");
+	        System.setProperty("https.proxyHost", "169.169.4.85");
+	        System.setProperty("https.proxyPort", "8080"); */
+			
+			//crea objeto de negocio
+			final ContratoNegocio negocio = new ContratoNegocio();
+			
+			ContratoDTO contrato = new ContratoDTO();
+			contrato.setNumeroContrato(numeroContrato);
+			contrato.setIdEmpleado(idEmpleado);
+			contrato.setIdVialidad(idVialidad);
+			contrato.setTramoInicial(tramoInicial);
+			contrato.setTramoFinal(tramoFinal);
+			respuesta = negocio.altaContrato(contrato);
+			//convierte  a formato Json
+			out.println(sg.toJson(respuesta));
+			out.flush();
 			
 		} catch (Exception e) {
 			LogHandler.error("", this.getClass(), "Error servlet", e);
