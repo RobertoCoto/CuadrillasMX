7ba7c8e7f1cd83d8461bd8249a4107c0fa4ac2bc
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-29 13:20:12 -0600

    se agrega DAO contrato

---
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/ContratoDAO.java
@@ -0,0 +1,47 @@
+package com.fyg.cuadrillas.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
+import com.fyg.cuadrillas.comun.ExcepcionesCuadrillas;
+import com.fyg.cuadrillas.comun.LogHandler;
+import com.fyg.cuadrillas.dto.contrato.ContratoDTO;
+
+public class ContratoDAO {
+	/**
+	 * Metodo para dar de alta un contrato
+	 * @param uid unico de registro
+	 * @param contrato recoibe valores de contrato
+	 * @return regresa la respuesta
+	 */
+	public EncabezadoRespuesta altaContrato(String uid, ContratoDTO contrato) {
+		SqlSession sessionTx = null;
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		respuesta.setUid(uid);
+		respuesta.setEstatus(true);
+		respuesta.setMensajeFuncional("registro correcto.");
+		try {
+			//Abrimos conexion Transaccional
+			sessionTx = FabricaConexiones.obtenerSesionTx();
+	        int registros = sessionTx.insert("ContratoDAO.altaContrato", contrato);
+			if ( registros == 0) {
+				throw new ExcepcionesCuadrillas("Error al registrar el contrato.");
+			}
+			//Realizamos commit
+			LogHandler.debug(uid, this.getClass(), "Commit!!!");
+			sessionTx.commit();
+			
+		} catch (Exception ex) {
+			//Realizamos rollBack
+			LogHandler.debug(uid, this.getClass(), "RollBack!!");
+			FabricaConexiones.rollBack(sessionTx);
+			LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
+			respuesta.setEstatus(false);
+			respuesta.setMensajeFuncional(ex.getMessage());
+		}
+		finally {
+			FabricaConexiones.close(sessionTx);
+		}
+		return respuesta;
+	}
+}
