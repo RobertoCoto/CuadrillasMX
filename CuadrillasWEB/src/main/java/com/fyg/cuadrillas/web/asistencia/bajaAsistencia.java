825fdb7745f07de4882e1386ab1c53322c8cc165
RobertoCoto <roberto.coto@fygsolutions.com>
2016-11-30 12:14:38 -0600

    bajaAsistencia

---
+++ CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/asistencia/bajaAsistencia.java
@@ -0,0 +1,79 @@
+package com.fyg.cuadrillas.web.asistencia;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
+import com.fyg.cuadrillas.comun.LogHandler;
+import com.fyg.cuadrillas.dto.asistencia.AsistenciaDTO;
+import com.fyg.cuadrillas.negocio.AsistenciaNegocio;
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class bajaAsistencia
+ */
+public class bajaAsistencia extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public bajaAsistencia() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		Gson sg = new Gson();
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		try {
+			Integer idEmpleado = Integer.parseInt(request.getParameter("idEmpleado"));
+			String usuario = request.getParameter("usuario");
+			
+			/* descomentar para proxy FISA
+			System.setProperty("http.proxyHost", "169.169.4.85");
+	        System.setProperty("http.proxyPort", "8080");
+	        System.setProperty("https.proxyHost", "169.169.4.85");
+	        System.setProperty("https.proxyPort", "8080"); */
+			
+			//crea objeto de negocio
+			final AsistenciaNegocio  negocio = new AsistenciaNegocio();
+			
+			
+			AsistenciaDTO asistencia = new AsistenciaDTO();
+			asistencia.setIdEmpleado(idEmpleado);
+			asistencia.setUsuarioBaja(usuario);
+			asistencia.setUsuarioUltMod(usuario);
+			respuesta = negocio.bajaAsistencia(asistencia);
+			//convierte  a formato Json
+			out.println(sg.toJson(respuesta));
+			out.flush();
+			
+		} catch (Exception e) {
+			LogHandler.error("", this.getClass(), "Error servlet", e);
+			respuesta.setMensajeFuncional("Error: " + e.getMessage());
+			respuesta.setEstatus(false);
+			out.println(sg.toJson(respuesta));
+			out.flush();
+		}
+	}
+
+}
