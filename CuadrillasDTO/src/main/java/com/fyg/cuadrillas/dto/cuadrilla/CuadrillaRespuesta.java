8123c2b1ecfff959828942cfa133f9edfa2077d0
RobertoCoto <roberto.coto@fygsolutions.com>
2016-12-01 16:45:30 -0600

    cuadrilla respuesta

--- CuadrillasDTO/src/main/java/com/fyg/cuadrillas/dto/cuadrilla/CuadrillaRespuesta.java
+++ CuadrillasDTO/src/main/java/com/fyg/cuadrillas/dto/cuadrilla/CuadrillaRespuesta.java
@@ -1,5 +1,42 @@
 package com.fyg.cuadrillas.dto.cuadrilla;
 
-public class CuadrillaRespuesta {
+import java.util.List;
 
+import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
+import com.fyg.cuadrillas.comun.ObjetoValor;
+
+public class CuadrillaRespuesta extends ObjetoValor {
+
+	/**
+	 * Serial UID
+	 */
+	private static final long serialVersionUID = -4102824258640948530L;
+	/** respuesta */
+	private EncabezadoRespuesta header;
+	/** lista*/
+	private List<CuadrillaDTO> cuadrilla;
+	/**
+	 * @return the header
+	 */
+	public EncabezadoRespuesta getHeader() {
+		return header;
+	}
+	/**
+	 * @param header the header to set
+	 */
+	public void setHeader(EncabezadoRespuesta header) {
+		this.header = header;
+	}
+	/**
+	 * @return the cuadrilla
+	 */
+	public List<CuadrillaDTO> getCuadrilla() {
+		return cuadrilla;
+	}
+	/**
+	 * @param cuadrilla the cuadrilla to set
+	 */
+	public void setCuadrilla(List<CuadrillaDTO> cuadrilla) {
+		this.cuadrilla = cuadrilla;
+	}
 }
