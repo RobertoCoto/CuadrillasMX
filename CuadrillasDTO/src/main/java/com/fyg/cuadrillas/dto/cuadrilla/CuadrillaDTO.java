8f210852a7f691cb0d084a9c7d953a4ec8c7fbce
RobertoCoto <roberto.coto@fygsolutions.com>
2016-12-01 16:37:00 -0600

    cuadrilla dto

--- CuadrillasDTO/src/main/java/com/fyg/cuadrillas/dto/cuadrilla/CuadrillaDTO.java
+++ CuadrillasDTO/src/main/java/com/fyg/cuadrillas/dto/cuadrilla/CuadrillaDTO.java
@@ -1,5 +1,7 @@
 package com.fyg.cuadrillas.dto.cuadrilla;
 
+import java.util.Date;
+
 import com.fyg.cuadrillas.comun.ObjetoValor;
 
 public class CuadrillaDTO extends ObjetoValor {
@@ -8,6 +10,308 @@ public class CuadrillaDTO extends ObjetoValor {
 	 * Serial UID
 	 */
 	private static final long serialVersionUID = -7807389711061036224L;
+	/**
+	 * id de la cuadrilla
+	 */
 	private Integer idCuadrilla;
-
+	/**
+	 * nombre de la cuadrilla
+	 */
+	private String nombreCuadrilla;
+	/**
+	 * id de la vialidad
+	 */
+	private Integer idVialidad;
+	/**
+	 * residente
+	 */
+	private String residente;
+	/**
+	 * numero de personas
+	 */
+	private Integer numeroPersonas;
+	/**
+	 * calificacion
+	 */
+	private Integer calificacion;
+	/**
+	 * latitud
+	 */
+	private float latitudInicial;
+	/**
+	 * direccion inicial
+	 */
+	private String direccionInicial;
+	/**
+	 * direccion final
+	 */
+	private String direccionFinal;
+	/**
+	 * longitud inicial
+	 */
+	private float longitudInicial;
+	/**
+	 * latitud final
+	 */
+	private float latitudFinal;
+	/**
+	 * longitud final
+	 */
+	private float longitudFinal;
+	/**
+	 * estatus
+	 */
+	private String estatus;
+	/**
+	 * usuario Alta
+	 */
+	private String usuarioAlta;
+	/**
+	 * fecha Alta
+	 */
+	private Date fechaAlta;
+	/**
+	 * usuario baja
+	 */
+	private String usuarioBaja;
+	/**
+	 * fecha baja
+	 */
+	private Date fechaBaja;
+	/**
+	 * fecha ult mod
+	 */
+	private Date fechaUltMod;
+	/**
+	 * usuario ultima mod
+	 */
+	private String usuarioUltMod;
+	/**
+	 * @return the idCuadrilla
+	 */
+	public Integer getIdCuadrilla() {
+		return idCuadrilla;
+	}
+	/**
+	 * @param idCuadrilla the idCuadrilla to set
+	 */
+	public void setIdCuadrilla(Integer idCuadrilla) {
+		this.idCuadrilla = idCuadrilla;
+	}
+	/**
+	 * @return the nombreCuadrilla
+	 */
+	public String getNombreCuadrilla() {
+		return nombreCuadrilla;
+	}
+	/**
+	 * @param nombreCuadrilla the nombreCuadrilla to set
+	 */
+	public void setNombreCuadrilla(String nombreCuadrilla) {
+		this.nombreCuadrilla = nombreCuadrilla;
+	}
+	/**
+	 * @return the idVialidad
+	 */
+	public Integer getIdVialidad() {
+		return idVialidad;
+	}
+	/**
+	 * @param idVialidad the idVialidad to set
+	 */
+	public void setIdVialidad(Integer idVialidad) {
+		this.idVialidad = idVialidad;
+	}
+	/**
+	 * @return the residente
+	 */
+	public String getResidente() {
+		return residente;
+	}
+	/**
+	 * @param residente the residente to set
+	 */
+	public void setResidente(String residente) {
+		this.residente = residente;
+	}
+	/**
+	 * @return the numeroPersonas
+	 */
+	public Integer getNumeroPersonas() {
+		return numeroPersonas;
+	}
+	/**
+	 * @param numeroPersonas the numeroPersonas to set
+	 */
+	public void setNumeroPersonas(Integer numeroPersonas) {
+		this.numeroPersonas = numeroPersonas;
+	}
+	/**
+	 * @return the calificacion
+	 */
+	public Integer getCalificacion() {
+		return calificacion;
+	}
+	/**
+	 * @param calificacion the calificacion to set
+	 */
+	public void setCalificacion(Integer calificacion) {
+		this.calificacion = calificacion;
+	}
+	/**
+	 * @return the latitudInicial
+	 */
+	public float getLatitudInicial() {
+		return latitudInicial;
+	}
+	/**
+	 * @param latitudInicial the latitudInicial to set
+	 */
+	public void setLatitudInicial(float latitudInicial) {
+		this.latitudInicial = latitudInicial;
+	}
+	/**
+	 * @return the direccionInicial
+	 */
+	public String getDireccionInicial() {
+		return direccionInicial;
+	}
+	/**
+	 * @param direccionInicial the direccionInicial to set
+	 */
+	public void setDireccionInicial(String direccionInicial) {
+		this.direccionInicial = direccionInicial;
+	}
+	/**
+	 * @return the direccionFinal
+	 */
+	public String getDireccionFinal() {
+		return direccionFinal;
+	}
+	/**
+	 * @param direccionFinal the direccionFinal to set
+	 */
+	public void setDireccionFinal(String direccionFinal) {
+		this.direccionFinal = direccionFinal;
+	}
+	/**
+	 * @return the longitudInicial
+	 */
+	public float getLongitudInicial() {
+		return longitudInicial;
+	}
+	/**
+	 * @param longitudInicial the longitudInicial to set
+	 */
+	public void setLongitudInicial(float longitudInicial) {
+		this.longitudInicial = longitudInicial;
+	}
+	/**
+	 * @return the latitudFinal
+	 */
+	public float getLatitudFinal() {
+		return latitudFinal;
+	}
+	/**
+	 * @param latitudFinal the latitudFinal to set
+	 */
+	public void setLatitudFinal(float latitudFinal) {
+		this.latitudFinal = latitudFinal;
+	}
+	/**
+	 * @return the longitudFinal
+	 */
+	public float getLongitudFinal() {
+		return longitudFinal;
+	}
+	/**
+	 * @param longitudFinal the longitudFinal to set
+	 */
+	public void setLongitudFinal(float longitudFinal) {
+		this.longitudFinal = longitudFinal;
+	}
+	/**
+	 * @return the estatus
+	 */
+	public String getEstatus() {
+		return estatus;
+	}
+	/**
+	 * @param estatus the estatus to set
+	 */
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+	/**
+	 * @return the usuarioAlta
+	 */
+	public String getUsuarioAlta() {
+		return usuarioAlta;
+	}
+	/**
+	 * @param usuarioAlta the usuarioAlta to set
+	 */
+	public void setUsuarioAlta(String usuarioAlta) {
+		this.usuarioAlta = usuarioAlta;
+	}
+	/**
+	 * @return the fechaAlta
+	 */
+	public Date getFechaAlta() {
+		return fechaAlta;
+	}
+	/**
+	 * @param fechaAlta the fechaAlta to set
+	 */
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+	/**
+	 * @return the usuarioBaja
+	 */
+	public String getUsuarioBaja() {
+		return usuarioBaja;
+	}
+	/**
+	 * @param usuarioBaja the usuarioBaja to set
+	 */
+	public void setUsuarioBaja(String usuarioBaja) {
+		this.usuarioBaja = usuarioBaja;
+	}
+	/**
+	 * @return the fechaBaja
+	 */
+	public Date getFechaBaja() {
+		return fechaBaja;
+	}
+	/**
+	 * @param fechaBaja the fechaBaja to set
+	 */
+	public void setFechaBaja(Date fechaBaja) {
+		this.fechaBaja = fechaBaja;
+	}
+	/**
+	 * @return the fechaUltMod
+	 */
+	public Date getFechaUltMod() {
+		return fechaUltMod;
+	}
+	/**
+	 * @param fechaUltMod the fechaUltMod to set
+	 */
+	public void setFechaUltMod(Date fechaUltMod) {
+		this.fechaUltMod = fechaUltMod;
+	}
+	/**
+	 * @return the usuarioUltMod
+	 */
+	public String getUsuarioUltMod() {
+		return usuarioUltMod;
+	}
+	/**
+	 * @param usuarioUltMod the usuarioUltMod to set
+	 */
+	public void setUsuarioUltMod(String usuarioUltMod) {
+		this.usuarioUltMod = usuarioUltMod;
+	}
 }
