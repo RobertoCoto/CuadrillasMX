b9e2b6f61bea0acda833b6992086829552723878
RobertoCoto <roberto.coto@fygsolutions.com>
2016-12-01 17:41:52 -0600

    CuadrillaDAO

--- CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/CuadrillaDAO.java
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/CuadrillaDAO.java
@@ -8,7 +8,12 @@ import com.fyg.cuadrillas.comun.LogHandler;
 import com.fyg.cuadrillas.dto.cuadrilla.CuadrillaDTO;
 
 public class CuadrillaDAO {
-
+	/**
+	 * Metodo para dar de alta una cuadrilla
+	 * @param uid unico de registro
+	 * @param cuadrilla recibe valores de cuadrilla
+	 * @return regresa respuesta
+	 */
 	public EncabezadoRespuesta altaCuadrilla (String uid, CuadrillaDTO cuadrilla) {
 		SqlSession sessionTx = null;
 		SqlSession sessionNTx = null;
@@ -21,8 +26,17 @@ public class CuadrillaDAO {
 			sessionNTx = FabricaConexiones.obtenerSesionNTx();
 			int existeMismaCuadrilla= (Integer) sessionNTx.selectOne("CuadrillaDAO.existeMismaCuadrilla", cuadrilla);
 			if (existeMismaCuadrilla > 0) {
-				throw new ExcepcionesCuadrillas("Error al registrar, ya existe un contrato vigente.");
+				throw new ExcepcionesCuadrillas("Error al registrar, ya existe la cuadrilla.");
 			}
+			//Abrimos conexion Transaccional
+			sessionTx = FabricaConexiones.obtenerSesionTx();
+	        int registros = sessionTx.insert("CuadrillaDAO.altaCuadrilla", cuadrilla);
+			if ( registros == 0) {
+				throw new ExcepcionesCuadrillas("Error al registrar una cuadrilla.");
+			}
+			//Realizamos commit
+			LogHandler.debug(uid, this.getClass(), "Commit!!!");
+			sessionTx.commit();
 		} catch (Exception ex) {
 			//Realizamos rollBack
 			LogHandler.debug(uid, this.getClass(), "RollBack!!");
@@ -36,5 +50,51 @@ public class CuadrillaDAO {
 			FabricaConexiones.close(sessionNTx);
 		}
 		return respuesta;
+	}
+	
+	/**
+	 * Metodo para dar de baja una cuadrilla
+	 * @param uid unico de registro
+	 * @param cuadrilla recibe valores de cuadrilla
+	 * @return regresa respuesta
+	 */
+	public EncabezadoRespuesta bajaCuadrilla (String uid, CuadrillaDTO cuadrilla) {
+		SqlSession sessionTx = null;
+		SqlSession sessionNTx = null;
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		respuesta.setUid(uid);
+		respuesta.setEstatus(true);
+		respuesta.setMensajeFuncional("inactivación correcta.");
+		
+		try {
+			//Validamos si ya esta dado de baja el contrato
+			sessionNTx = FabricaConexiones.obtenerSesionNTx();
+			int existeBaja= (Integer) sessionNTx.selectOne("CuadrillaDAO.existeBaja", cuadrilla);
+			if (existeBaja > 0) {
+				throw new ExcepcionesCuadrillas("Error al dar de baja, la cuadrilla ya se encuentra inactiva.");
+			}
+			//Abrimos conexion Transaccional
+			sessionTx = FabricaConexiones.obtenerSesionTx();
+	        int registros = sessionTx.insert("CuarillaDAO.bajaContrato", cuadrilla);
+			if ( registros == 0) {
+				throw new ExcepcionesCuadrillas("Error al dar de baja la cuadrilla.");
+			}
+			//Realizamos commit
+			LogHandler.debug(uid, this.getClass(), "Commit!!!");
+			sessionTx.commit();	
+		}
+		 catch (Exception ex) {
+				//Realizamos rollBack
+				LogHandler.debug(uid, this.getClass(), "RollBack!!");
+				FabricaConexiones.rollBack(sessionTx);
+				LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
+				respuesta.setEstatus(false);
+				respuesta.setMensajeFuncional(ex.getMessage());
+			}
+			finally {
+				FabricaConexiones.close(sessionTx);
+				FabricaConexiones.close(sessionNTx);
+			}
+			return respuesta;
 	}
 }
