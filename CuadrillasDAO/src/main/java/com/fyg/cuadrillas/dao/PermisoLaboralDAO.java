e5aaf13b0cd3b8781fdaabc7691dff757327359e
RobertoCoto <roberto.coto@fygsolutions.com>
2016-12-05 12:41:54 -0600

    cambios

--- CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/PermisoLaboralDAO.java
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/PermisoLaboralDAO.java
@@ -79,4 +79,41 @@ public class PermisoLaboralDAO {
 		}
 		return respuesta;
 	}
+	/**
+	 * Metodo para dar de baja un permiso
+	 * @param uid unico de registro
+	 * @param permiso recibe valores de permiso
+	 * @return regresa  una respuesta
+	 */
+	public EncabezadoRespuesta bajaPermiso(String uid, PermisoLaboralDTO permiso) {
+		SqlSession sessionTx = null;
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		respuesta.setUid(uid);
+		respuesta.setEstatus(true);
+		respuesta.setMensajeFuncional("baja correcta.");
+		
+		try {
+			//Abrimos conexion Transaccional
+			sessionTx = FabricaConexiones.obtenerSesionTx();
+	        int registros = sessionTx.insert("PermisoLaboralDAO.bajaPermiso", permiso);
+			if ( registros == 0) {
+				throw new ExcepcionesCuadrillas("Error al inhabilitar el permiso.");
+			}
+			//Realizamos commit
+			LogHandler.debug(uid, this.getClass(), "Commit!!!");
+			sessionTx.commit();
+			
+		} catch (Exception ex) {
+			//Realizamos rollBack
+			LogHandler.debug(uid, this.getClass(), "RollBack!!");
+			FabricaConexiones.rollBack(sessionTx);
+			LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
+			respuesta.setEstatus(false);
+			respuesta.setMensajeFuncional(ex.getMessage());
+		}
+		finally {
+			FabricaConexiones.close(sessionTx);
+		}
+		return respuesta;
+	}
 }
