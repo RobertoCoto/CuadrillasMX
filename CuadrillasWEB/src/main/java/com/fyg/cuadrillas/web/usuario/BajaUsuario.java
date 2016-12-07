829d4a33dbcb0e4eb6601a5ad7e714cb12621f40
RobertoCoto <roberto.coto@fygsolutions.com>
2016-12-07 17:46:07 -0600

    servlet baja usuario

--- CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/usuario/BajaUsuario.java
+++ CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/usuario/BajaUsuario.java
@@ -1,10 +1,18 @@
 package com.fyg.cuadrillas.web.usuario;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
+import com.fyg.cuadrillas.comun.LogHandler;
+import com.fyg.cuadrillas.dto.usuario.UsuarioDTO;
+import com.fyg.cuadrillas.negocio.UsuariosNegocio;
+import com.google.gson.Gson;
 
 /**
  * Servlet implementation class BajaUsuario
@@ -24,14 +32,45 @@ public class BajaUsuario extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		this.doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		Gson sg = new Gson();
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		try {
+			String user = request.getParameter("user");
+			
+			/* descomentar para proxy FISA
+			System.setProperty("http.proxyHost", "169.169.4.85");
+	        System.setProperty("http.proxyPort", "8080");
+	        System.setProperty("https.proxyHost", "169.169.4.85");
+	        System.setProperty("https.proxyPort", "8080"); */
+			
+			 //crea objeto del negocio usuario
+	        final UsuariosNegocio negocio = new UsuariosNegocio();
+	        
+	        //Login usuario
+	        UsuarioDTO usuario = new UsuarioDTO();
+	        usuario.setUsuario(user);
+	        respuesta = negocio.bajaUsuario(usuario);
+	      //convierte  a formato Json
+			out.println(sg.toJson(respuesta));
+			out.flush();
+			
+		} catch(Exception e) {
+			LogHandler.error("", this.getClass(), "Error servlet", e);
+			respuesta.setMensajeFuncional("Error: " + e.getMessage());
+			respuesta.setEstatus(false);
+			out.println(sg.toJson(respuesta));
+			out.flush();
+		}
 	}
 
 }
