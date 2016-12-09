b055af9fbb826a6410d57c581ec5a40ef3c5f532
RobertoCoto <roberto.coto@fygsolutions.com>
2016-12-09 17:46:02 -0600

    DAO actividad

---
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/ActividadDAO.java
@@ -0,0 +1,46 @@
+package com.fyg.cuadrillas.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
+import com.fyg.cuadrillas.comun.ExcepcionesCuadrillas;
+import com.fyg.cuadrillas.comun.LogHandler;
+import com.fyg.cuadrillas.dto.actividad.ActividadDTO;
+
+public class ActividadDAO {
+	/**
+	 * Metodo para registrar la actividad diaria
+	 * @param uid unico de registro
+	 * @param actividad recibe los valores e actividad
+	 * @return regresa una respuesta
+	 */
+	public EncabezadoRespuesta registraActividad(String uid, ActividadDTO actividad) {
+		SqlSession sessionTx = null;
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		respuesta.setUid(uid);
+		respuesta.setEstatus(true);
+		respuesta.setMensajeFuncional("registro de entrada correcto.");
+		try {
+			//Abrimos conexion Transaccional
+			sessionTx = FabricaConexiones.obtenerSesionTx();
+	        int registros = sessionTx.insert("ActividadDAO.registraActividad", actividad);
+			if ( registros == 0) {
+				throw new ExcepcionesCuadrillas("No fue posible registrar la actividad diaria.");
+			}
+			//Realizamos commit
+			LogHandler.debug(uid, this.getClass(), "Commit!!!");
+			sessionTx.commit();
+		} catch (Exception ex) {
+			//Realizamos rollBack
+			LogHandler.debug(uid, this.getClass(), "RollBack!!");
+			FabricaConexiones.rollBack(sessionTx);
+			LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
+			respuesta.setEstatus(false);
+			respuesta.setMensajeFuncional(ex.getMessage());
+		}
+		finally {
+			FabricaConexiones.close(sessionTx);
+		}
+		return respuesta;
+	}
+}
