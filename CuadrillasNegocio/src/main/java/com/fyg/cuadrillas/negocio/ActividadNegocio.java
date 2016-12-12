38fa72ae9f9c8098518047c84b609a3f7dca27ef
RobertoCoto <roberto.coto@fygsolutions.com>
2016-12-12 17:59:58 -0600

    modif negocio actividad

--- CuadrillasNegocio/src/main/java/com/fyg/cuadrillas/negocio/ActividadNegocio.java
+++ CuadrillasNegocio/src/main/java/com/fyg/cuadrillas/negocio/ActividadNegocio.java
@@ -125,4 +125,46 @@ public class ActividadNegocio {
 				LogHandler.debug(uid, this.getClass(), "autorizaActividad - Datos Salida: " + respuesta);
 				return respuesta;
 	}
+	/**
+	 * metodo para dar de baja actividad
+	 * @param actividad recibe valores d actividad
+	 * @return regresa respuesta
+	 */
+	public EncabezadoRespuesta bajaActividad(ActividadDTO actividad) {
+		//Primero generamos el identificador unico de la transaccion
+		String uid = GUIDGenerator.generateGUID(actividad);
+		//Mandamos a log el objeto de entrada
+		LogHandler.debug(uid, this.getClass(), "bajaActividad - Datos Entrada: " + actividad);
+		//Variable de resultado
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		
+		try {
+			if (actividad.getIdActividad() == null) {
+				throw new ExcepcionesCuadrillas("Es necesario especificar el id de la actividad.");
+			}
+			if (actividad.getIdCuadrilla() == null) {
+				throw new ExcepcionesCuadrillas("Es necesario especificar el id de la cuadrilla.");
+			}
+			if(actividad.getUsuarioBaja() == null || actividad.getUsuarioBaja().trim().isEmpty()) {
+				throw new ExcepcionesCuadrillas("Es necesario especificar el usuario.");
+			}
+			ActividadDAO dao = new ActividadDAO();
+			respuesta = dao.bajaActividad(uid, actividad);
+		} catch  (ExcepcionesCuadrillas ex) {
+			LogHandler.error(uid, this.getClass(), "bajaActividad - Error: " + ex.getMessage(), ex);
+			respuesta.setUid(uid);
+			respuesta.setEstatus(false);
+			respuesta.setMensajeFuncional(ex.getMessage());
+			respuesta.setMensajeTecnico(ex.getMessage());
+		}
+		catch  (Exception ex) {
+			LogHandler.error(uid, this.getClass(), "bajaActividad - Error: " + ex.getMessage(), ex);
+			respuesta.setUid(uid);
+			respuesta.setEstatus(false);
+			respuesta.setMensajeFuncional(ex.getMessage());
+			respuesta.setMensajeTecnico(ex.getMessage());
+		}
+		LogHandler.debug(uid, this.getClass(), "bajaActividad - Datos Salida: " + respuesta);
+		return respuesta;
+	}
 }
