28b0609bb19e35f5b6941baf61768c8838726e69
RobertoCoto <roberto.coto@fygsolutions.com>
2016-12-12 17:44:48 -0600

    dao actividad

--- CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/ActividadDAO.java
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/ActividadDAO.java
@@ -89,4 +89,44 @@ public class ActividadDAO {
 		return respuesta;
 		
 	}
+	
+	public EncabezadoRespuesta bajaActividad (String uid, ActividadDTO actividad) {
+		SqlSession sessionTx = null;
+		SqlSession sessionNTx = null;
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		respuesta.setUid(uid);
+		respuesta.setEstatus(true);
+		respuesta.setMensajeFuncional("baja correcta.");
+		try {
+			//Validamos si ya esta inactivo
+			sessionNTx = FabricaConexiones.obtenerSesionNTx();
+			int existeBaja= (Integer) sessionNTx.selectOne("ActividadDAO.existeBajaActividad", actividad);
+			if (existeBaja > 0) {
+				throw new ExcepcionesCuadrillas("Error al modificar, la actividad ya esta dado de baja.");
+			}
+			//Abrimos conexion Transaccional
+			sessionTx = FabricaConexiones.obtenerSesionTx();
+	        int registros = sessionTx.insert("ActividadDAO.bajaActividad", actividad);
+			if ( registros == 0) {
+				throw new ExcepcionesCuadrillas("Error al dar de baja la actividad.");
+			}
+			//Realizamos commit
+			LogHandler.debug(uid, this.getClass(), "Commit!!!");
+			sessionTx.commit();
+			
+		}catch (Exception ex) {
+			//Realizamos rollBack
+			LogHandler.debug(uid, this.getClass(), "RollBack!!");
+			FabricaConexiones.rollBack(sessionTx);
+			LogHandler.error(uid, this.getClass(), "Error: " + ex.getMessage(), ex);
+			respuesta.setEstatus(false);
+			respuesta.setMensajeFuncional(ex.getMessage());
+		}
+		finally {
+			FabricaConexiones.close(sessionTx);
+			FabricaConexiones.close(sessionNTx);
+		}
+		return respuesta;
+		
+	}
 }
