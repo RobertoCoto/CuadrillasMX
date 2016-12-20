1d9c9304a8c746d7a0558690b2cb51ad982faae1
RobertoCoto <roberto.coto@fygsolutions.com>
2016-12-20 09:53:14 -0600

    cambios

--- parametros.sql
+++
@@ -1,9 +0,0 @@
-USE tatei;
-
-DELETE FROM parametros;
-
-INSERT INTO parametros
-	(	parametro,	valor,	fecha_alta,	fecha_ult_mod,	estatus	)
-VALUES 
-	(	'usuario.edad.ano.minimo',	'1900',	now(),	now(),	'A'	);
-
