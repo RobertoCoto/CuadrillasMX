e1c401a134e1dc21058a8087c9b442be264e1774
RobertoCoto <roberto.coto@fygsolutions.com>
2017-01-06 17:51:31 -0600

    cambios

--- CuadrillasWEB/src/main/webapp/empleado/js/registraEmpleado.js
+++ CuadrillasWEB/src/main/webapp/empleado/js/registraEmpleado.js
@@ -7,6 +7,18 @@ $scope.empleado = {};
 	   $scope.registrar = function(empleado) {
 	   $scope.datoempleado.push(empleado);
 	   console.log($scope.datoempleado);
+	   $scope.nombre = empleado.nombre;
+	   $scope.apellidoPaterno = empleado.apellidoPat;
+	   $scope.apellidoMaterno = empleado.apellidoMat;
+	   $scope.sexo = empleado.sexo;
+	   $scope.rfc = empleado.rfc;
+	   $scope.fechaIngreso = empleado.fechaIngreso;
+	   $http.post("http://localhost:8080/CuadrillasWEB/RegistraEmpleado?" + "nombre=" + $scope.nombre +"&apellidoPat="+$scope.apellidoPaterno
+		   ).success(function (dataSitio) {
+	    	 $scope.items = dataSitio;
+	    	
+		      });
 	   }
+	   
 	   
 	 });
