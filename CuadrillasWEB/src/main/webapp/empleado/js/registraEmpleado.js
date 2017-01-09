0d25e99396b7d82e9132810e1cd2cd16ab1499ed
RobertoCoto <roberto.coto@fygsolutions.com>
2017-01-09 17:37:55 -0600

    modificacion

--- CuadrillasWEB/src/main/webapp/empleado/js/registraEmpleado.js
+++ CuadrillasWEB/src/main/webapp/empleado/js/registraEmpleado.js
@@ -12,9 +12,26 @@ $scope.empleado = {};
 	   $scope.apellidoMaterno = empleado.apellidoMat;
 	   $scope.sexo = empleado.sexo;
 	   $scope.rfc = empleado.rfc;
+	   $scope.fechaNacimiento = empleado.fechaNacimiento;
 	   $scope.fechaIngreso = empleado.fechaIngreso;
-	   $http.post("http://localhost:8080/CuadrillasWEB/RegistraEmpleado?" + "nombre=" + $scope.nombre +"&apellidoPat="+$scope.apellidoPaterno
-		   ).success(function (dataSitio) {
+	   $scope.codigoPuesto = empleado.puesto;
+	   $scope.codigoVialidad = empleado.codigoVialidad;
+	   $scope.codigoArea = empleado.codigoArea;
+	   $scope.codigoTalla = empleado.codigoTalla;
+	   $scope.idCuadrilla = empleado.idCuadrilla;
+	   $scope.sueldo  = empleado.sueldo;
+	   $scope.frecuenciaPago = empleado.tipoPago;
+	   $scope.nss = empleado.nss;
+	   $scope.telefono = empleado.telefono;
+	   $scope.noCreditoInfonavit = empleado.infonavit;
+	   $scope.observaciones = empleado.comentarios;
+	   $scope.usuario = empleado.usuario;
+	   
+	   $http.post("http://localhost:8080/CuadrillasWEB/RegistraEmpleado?" + "nombre=" + $scope.nombre +"&apellidoPat="+$scope.apellidoPaterno +"&apellidoMat="
+		 +$scope.apellidoMaterno +"&sexo=" + $scope.sexo + "&rfc=" ´+ $scope.rfc + "&fechaNacimiento=" + $scope.fechaNacimiento + "&fechaIngreso="
+		 + $acope.fechaIngreso + "&codigoPuesto=" + $scope.codigoPuesto + "&codigoVialidad =" + $scope.codigoVialidad + "&codigoTalla=" + $scope.codigoTalla
+		 + "&idCuadrilla=" +$scope.idCuadrilla + "&sueldo=" +$scope.sueldo +"&frecuenciaPago=" + $scope.frecuenciaPago + "&nss=" +$scope.nss 
+		 + "&telefono=" + $scope.telefono + "&noCreditoInfonavit=" + $scope.noCreditoInfonavit + "&observaciones").success(function (dataSitio) {
 	    	 $scope.items = dataSitio;
 	    	
 		      });
