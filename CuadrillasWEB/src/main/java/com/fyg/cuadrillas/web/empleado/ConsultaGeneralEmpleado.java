2a3d97bcb3b66f5b258312260286c2a5bbf8991a
RobertoCoto <roberto.coto@fygsolutions.com>
2017-01-16 18:14:59 -0600

    Cambios por revision pantalla

---
+++ CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/empleado/ConsultaGeneralEmpleado.java
@@ -0,0 +1,73 @@
+package com.fyg.cuadrillas.web.empleado;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fyg.cuadrillas.comun.LogHandler;
+import com.fyg.cuadrillas.dto.empleado.EmpleadoRespuesta;
+import com.fyg.cuadrillas.negocio.EmpleadoNegocio;
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class ConsultaGeneralEmpleado
+ */
+public class ConsultaGeneralEmpleado extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConsultaGeneralEmpleado() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EmpleadoRespuesta respuesta = new EmpleadoRespuesta();
+		Gson sg = new Gson();
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		try {
+//			System.setProperty("http.proxyHost", "169.169.4.85");
+//	        System.setProperty("http.proxyPort", "8080");
+//	        System.setProperty("https.proxyHost", "169.169.4.85");
+//	        System.setProperty("https.proxyPort", "8080");
+			
+			//crea objeto de negocio
+			final EmpleadoNegocio negocio = new EmpleadoNegocio();
+			respuesta = negocio.consultaGeneral();
+			if (respuesta.getHeader().isEstatus()) {
+				response.setStatus(HttpServletResponse.SC_OK);
+			} else {
+				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			}
+			//convierte  a formato Json
+			out.println(sg.toJson(respuesta));
+			out.flush();
+		}catch (Exception e) {
+			LogHandler.error("", this.getClass(), "Error servlet", e);
+			respuesta.getHeader().setMensajeFuncional("Error: " + e.getMessage());
+			respuesta.getHeader().setEstatus(false);
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			out.println(sg.toJson(respuesta));
+			out.flush();
+		}
+	}
+
+}
