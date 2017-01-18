9ab44dfd2037e46d3bda390e89bc8e8f8fb229b7
RobertoCoto <roberto.coto@fygsolutions.com>
2017-01-18 15:49:41 -0600

    cambios

--- CuadrillasWEB/src/main/webapp/usuario/js/adminUser.js
+++ CuadrillasWEB/src/main/webapp/usuario/js/adminUser.js
@@ -1,2 +1,15 @@
 var app = angular.module('tatei', []);
-app.controller('cambioDatos', function ($scope, $http) { });
+app.controller('adminDatos', function ($scope, $http) { 
+	
+http({
+            method: 'GET',
+            url: 'http://localhost:8080/CuadrillasWEB/ConsultaGeneral',
+            data {}
+		    }).then(function (result) {
+		    	$scope.resultadoAdmin = result.data.usuario;
+	            console.log($scope.resultadoadmin);
+		    }, function myError(response) {
+		        console.error(response);
+		        alert(response.data.header.mensajeFuncional);
+		    });
+});
