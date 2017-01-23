c190e3721b45a34f93b5ec97f0bdf60091752de4
RobertoCoto <roberto.coto@fygsolutions.com>
2017-01-23 15:45:16 -0600

    se modifica js permiso

--- CuadrillasWEB/src/main/webapp/permiso/js/permisoLaboral.js
+++ CuadrillasWEB/src/main/webapp/permiso/js/permisoLaboral.js
@@ -1,10 +1,25 @@
 var app = angular.module('tatei', []);
 app.controller('registraPermiso', function ($scope, $http) {
+	//obtener el id enviado por GET
+	   $scope.obtainGet = function getGET(){
+		   var loc = document.location.href;
+		   var getString = loc.split('?')[1];
+		   var GET = getString.split('&');
+		   var get = {};
+
+		   for(var i = 0, l = GET.length; i < l; i++){
+		      var tmp = GET[i].split('=');
+		      get[tmp[0]] = unescape(decodeURI(tmp[1]));
+		   }
+		   return get;
+		};
+		$scope.get = $scope.obtainGet();
+		
 	$http({
         method: 'GET',
         url: 'http://localhost:8080/CuadrillasWEB/ConsultaEmpleado',
         params: {
-	 		"idEmpleado" : '1'
+	 		"idEmpleado" : $scope.get.idEmpleado
 	         },
         data: { }
 	    }).then(function (result) {
