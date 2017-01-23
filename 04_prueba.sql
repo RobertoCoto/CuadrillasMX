7393c02c6050584c6aceabd3191b95b66074d14c
RobertoCoto <roberto.coto@fygsolutions.com>
2017-01-23 11:52:37 -0600

    para pruebas (modificado)

--- 04_prueba.sql
+++ 04_prueba.sql
@@ -1,16 +1,8 @@
 /*DATOS EXTRA PARA PRUEBAS*/
 
-INSERT INTO `vialidad` (`id_vialidad`, `nombre`, `usuario_alta`, `fecha_alta`, `usuario_baja`, `fecha_baja`, `usuario_ult_mod`, `fecha_ult_mod`, `estatus`) VALUES
-(1, 'INSURGENTES', 'SISTEMAS', '2017-01-12 00:00:00', NULL, NULL, '', '0000-00-00 00:00:00', 'A'),
-(2, '5 DE MAYO', 'SISTEMAS', '2017-01-12 00:00:00', NULL, NULL, '', '0000-00-00 00:00:00', 'A');
-
-INSERT INTO `vialidad_coordenadas` (`id_coordenada`, `id_vialidad`, `latitud`, `longitud`, `fecha_alta`, `estatus`) VALUES
-(1, 1, 19.3673, -99.1758, '2017-01-12 00:00:00', 'A'),
-(2, 2, 19.4202, -99.1559, '2017-01-12 00:00:00', 'A');
-
-INSERT INTO `cuadrilla` (`id_cuadrilla`, `nombre_cuadrilla`, `id_vialidad`, `numero_personas`, `calificacion`, `fecha_alta`, `usuario_alta`, `fecha_baja`, `usuario_baja`, `usuario_ult_mod`, `fecha_ult_mod`, `estatus`) VALUES
-(1, 'Equipo insurgentes', 2, 11, 5, '2017-01-12 00:00:00', 'SISTEMAS', NULL, NULL, 'SISTEMAS', '2017-01-12 00:00:00', 'A'),
-(2, 'Equipo EJE 6', 1, 5, 5, '2017-01-12 00:00:00', 'SISTEMAS', NULL, NULL, 'SISTEMAS', '2017-01-12 00:00:00', 'A');
+INSERT INTO `cuadrilla` (`id_cuadrilla`, `nombre_cuadrilla`,`id_contrato`,`calificacion`, `fecha_alta`, `usuario_alta`, `fecha_baja`, `usuario_baja`, `usuario_ult_mod`, `fecha_ult_mod`, `estatus`) VALUES
+(1, 'Equipo insurgentes',1,5, '2017-01-12 00:00:00', 'SISTEMAS', NULL, NULL, 'SISTEMAS', '2017-01-12 00:00:00', 'A'),
+(2, 'Equipo EJE 6', 1, 5, '2017-01-12 00:00:00', 'SISTEMAS', NULL, NULL, 'SISTEMAS', '2017-01-12 00:00:00', 'A');
 
 INSERT INTO `empleado` (`id_empleado`, `no_empleado`, `id_cuadrilla`, `nombre`, `apellido_pat`, `apellido_mat`, `fecha_nacimiento`, `sexo`, `rfc`, `rfc_calculado`, `fecha_ingreso`, `sueldo`, `frecuencia_pago`, `nss`, `no_credito_infonavit`, `telefono`, `alta_imss`, `observaciones`, `codigo_puesto`, `codigo_vialidad`, `codigo_area`, `codigo_talla`, `usuario_alta`, `fecha_alta`, `usuario_baja`, `fecha_baja`, `codigo_tipo_salida`, `codigo_causa_salida`, `usuario_ult_mod`, `fecha_ult_mod`, `estatus`) VALUES
 (1, '0394393004', 1, 'ROBERTO ANTONIO', 'COTO', 'IXTEPAN', '1987-01-28', 'H', 'COIR920801JE9', 'COIR920801JE9', '2016-09-01', '2000.00', 'M', NULL, NULL, '557904955', 'S', 'EXCELENTE', 'CHOF', 'EJE6', '5MAY', 'EXCH', 'SISTEMAS', '2016-09-15 00:00:00', NULL, NULL, NULL, NULL, 'SISTEMAS', '2017-01-12 00:00:00', 'A');
