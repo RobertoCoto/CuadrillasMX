36474f90dc39f209f05669ddea7ba3ec8bd85626
RobertoCoto <roberto.coto@fygsolutions.com>
2017-01-26 16:08:02 -0600

    cambio en validacion html

--- CuadrillasWEB/src/main/webapp/cuadrillas/js/altaCuadrilla.js
+++ CuadrillasWEB/src/main/webapp/cuadrillas/js/altaCuadrilla.js
@@ -1,4 +1,19 @@
 var app = angular.module('tatei', []);
 app.controller('adminCuad', function ($scope, $http) {
 	
+ $http({
+              method: 'GET',
+              url: 'http://localhost:8080/CuadrillasWEB/ConsultaCatalogo',
+              params : {
+		 		"tipoCatalogo": 'VIALIDAD'
+		 },
+              data: { }
+		    }).then(function mySucces(result) {
+		    	$scope.resultado = result.data.catalogo;
+	              console.log(result);
+		    }, function myError(response) {
+		        console.error(response);
+		        alert(response.data.mensajeFuncional);
+		        //$scope.resultado2.push(objecto);
+		    });
 	});
