e0c0aad50ef19b696f52c42ec15d5cb59c42deab
RobertoCoto <roberto.coto@fygsolutions.com>
2017-02-02 10:26:10 -0600

    cambios

--- CuadrillasWEB/src/main/webapp/empleado/js/registraEmpleado.js
+++ CuadrillasWEB/src/main/webapp/empleado/js/registraEmpleado.js
@@ -673,4 +673,178 @@ $scope.JSONDocumentationNaDocsUpdate = JSON.stringify( { 'documentacion' : $scop
 		    
     	   };
     	   
+    $scope.consultaEmpleado = function(general) {
+    	  $scope.idEm = general.idEmpleado;
+   	   console.log("id del editar " + $scope.idEm);
+   	   console.log("la calificacion shabo" + general.calificacion);
+   	  
+		   var codigo = null;
+					$http({
+					    method: 'GET',
+					    url: 'http://localhost:8080/CuadrillasWEB/ConsultaDocumentos',
+					    params : {
+							"idEmpleado": $scope.idEm
+					}
+					  }).then(function mySucces(result) {
+					  	$scope.resultadoDocumentoEmp = result.data.empleadoDocumento;
+
+					  }, function myError(response) {
+					      console.error(response);
+					      alert(response.data.mensajeFuncional);
+					      //$scope.resultado2.push(objecto);
+					  });
+
+            document.getElementById("idData").value= $scope.idEm;
+            
+           
+   	   
+   	   document.getElementById("noEmpleado").value = general.noEmpleado;
+   	   document.getElementById("nombre").value = general.nombre;
+   	   document.getElementById("apellidoPaterno").value = general.apellidoPat;
+   	   document.getElementById("apellidoMaterno").value = general.apellidoMat;
+   	   document.getElementById("fechaNacimiento").value = general.fechaNacimiento;
+   	   document.getElementById("nss").value = general.nss;
+   	   document.getElementById("telefono").value = general.telefono;
+   	   document.getElementById("rfc").value = general.rfc;
+   	   document.getElementById("calificacion").value = general.calificacion;
+   	   document.getElementById("noCreditoInfonavit").value = general.noCreditoInfonavit;
+   	   document.getElementById("sueldo").value = general.sueldo;
+   	   $scope.cuadri = general.idCuadrilla;
+   	   document.getElementById('idCuadrilla').value="number:"+ $scope.cuadri;
+   	   document.getElementById("cCuadrilla").value = $scope.cuadri;
+   	   //validacion del sexo
+   	   if(general.sexo == "M")
+   	   { 
+   		   console.log(general.sexo);
+   		   document.getElementById("sexo").selectedIndex = "1";
+   		   
+   		} else if(general.sexo == "F")
+   	   {
+   		   document.getElementById("sexo").selectedIndex = "2";
+   		   }
+   	   
+   		   //validacion del puesto
+   	   
+   	    if(general.codigoPuesto == "AYGE") 
+   	    {
+   	    	console.log(general.codigoPuesto);
+   	    	document.getElementById("codigoPuesto").selectedIndex = "1";
+   	    	document.getElementById("cPuesto").value = general.codigoPuesto;
+   	    }
+   	    if(general.codigoPuesto == "CABO") 
+   	    {
+   	    	console.log(general.codigoPuesto);
+   	    	document.getElementById("codigoPuesto").selectedIndex = "2";
+   	    	document.getElementById("cPuesto").value = general.codigoPuesto;
+   	    }
+   	    if(general.codigoPuesto == "CHOF") 
+   	    {
+   	    	console.log(general.codigoPuesto);
+   	    	document.getElementById("codigoPuesto").selectedIndex = "3";
+   	    	document.getElementById("cPuesto").value = general.codigoPuesto;
+   	    }
+   	     if(general.codigoPuesto == "MAQU") 
+   	    {
+   	    	console.log(general.codigoPuesto);
+   	    	document.getElementById("codigoPuesto").selectedIndex = "4";
+   	    	document.getElementById("cPuesto").value = general.codigoPuesto;
+   	    }
+   	    if(general.codigoPuesto == "OFCE") 
+   	    {
+   	    	console.log(general.codigoPuesto);
+   	    	document.getElementById("codigoPuesto").selectedIndex = "5";
+   	    	document.getElementById("cPuesto").value = general.codigoPuesto;
+   	    }
+   	    if(general.codigoPuesto == "PODA") 
+   	    {
+   	    	console.log(general.codigoPuesto);
+   	    	document.getElementById("codigoPuesto").selectedIndex = "6";
+   	    	document.getElementById("cPuesto").value = general.codigoPuesto;
+   	    }
+   	    if(general.codigoPuesto == "RESI") 
+   	    {
+   	    	console.log(general.codigoPuesto);
+   	    	document.getElementById("codigoPuesto").selectedIndex = "7";
+   	    	document.getElementById("cPuesto").value = general.codigoPuesto;
+   	    }
+   	    
+   		 //validacion de talla
+        if(general.codigoTalla == "CHIC") 
+   	    {
+   	    	console.log(general.codigoTalla);
+   	    	document.getElementById("codigoTalla").selectedIndex = "1";
+   	    	document.getElementById("cTalla").value = general.codigoTalla;
+   	    }
+   	   if(general.codigoTalla == "GRAN") 
+   	    {
+   	    	console.log(general.codigoTalla);
+   	    	document.getElementById("codigoTalla").selectedIndex = "2";
+   	    	document.getElementById("cTalla").value = general.codigoTalla;
+   	    }
+   	    if(general.codigoTalla == "MEDI") 
+   	    {
+   	    	console.log(general.codigoTalla);
+   	    	document.getElementById("codigoTalla").selectedIndex = "3";
+   	    	document.getElementById("cTalla").value = general.codigoTalla;
+   	    }
+   	    if(general.codigoTalla == "EXCH") 
+   	    {
+   	    	console.log(general.codigoTalla);
+   	    	document.getElementById("codigoTalla").selectedIndex = "4";
+   	    	document.getElementById("cTalla").value = general.codigoTalla;
+   	    }
+   	    if(general.codigoTalla == "EXGR") 
+   	    {
+   	    	console.log(general.codigoTalla);
+   	    	document.getElementById("codigoTalla").selectedIndex = "5";
+   	    	document.getElementById("cTalla").value = general.codigoTalla;
+   	    }
+   	    
+   	    //validacion area
+   	    if(general.codigoArea == "VERD") 
+   	    {
+   	    	console.log(general.codigoArea);
+   	    	document.getElementById("codigoArea").selectedIndex = "1";
+   	    	document.getElementById("cArea").value = general.codigoArea;
+   	    }
+   	    
+   	    //validacion vialidad
+             if(general.codigoVialidad == "5MAY") 
+   	    {
+   	    	console.log(general.codigoVialidad);
+   	    	document.getElementById("codigoVialidad").selectedIndex = "1";
+   	    }
+   	     if(general.codigoVialidad == "EJE6") 
+   	    {
+   	    	console.log(general.codigoVialidad);
+   	    	document.getElementById("codigoVialidad").selectedIndex = "2";
+   	    }
+   	     if(general.codigoVialidad == "EJE7") 
+   	    {
+   	    	console.log(general.codigoVialidad);
+   	    	document.getElementById("codigoVialidad").selectedIndex = "3";
+   	    }
+   	     if(general.codigoVialidad == "INSU") 
+   	    {
+   	    	console.log(general.codigoVialidad);
+   	    	document.getElementById("codigoVialidad").selectedIndex = "4";
+   	    }
+   	     if(general.codigoVialidad == "UNIV") 
+   	    {
+   	    	console.log(general.codigoVialidad);
+   	    	document.getElementById("codigoVialidad").selectedIndex = "5";
+   	    }
+   	     if(general.codigoVialidad == "ZARA") 
+   	    {
+   	    	console.log(general.codigoVialidad);
+   	    	document.getElementById("codigoVialidad").selectedIndex = "6";
+   	    }
+   	   
+   	   document.getElementById('editar').style.display='none'; 
+   	  $("div[data-id=documentosData").hide();
+   	  $("div[data-id=documentosResultado").show();
+   	   document.getElementById('guardarDato').style.display='block'; 
+    	};
+    
+    	   
 	 });
