149d5f254b36ce76d4e1f9cc07904586575d4a89
RobertoCoto <roberto.coto@fygsolutions.com>
2017-02-03 18:33:05 -0600

    cambios asistencia

--- CuadrillasWEB/src/main/webapp/asistencia/js/entradaAsistencia.js
+++ CuadrillasWEB/src/main/webapp/asistencia/js/entradaAsistencia.js
@@ -1,20 +1,7 @@
 var app = angular.module('tatei', []);
 app.controller('entradaAsistencia', function ($scope, $http) {  
 	
-	$http({
-              method: 'GET',
-              url: 'http://localhost:8080/CuadrillasWEB/ConsultaEmpleado',
-              params: {
-		 		"idEmpleado" : '1'
-		         },
-              data: { }
-		    }).then(function (result) {
-		    	$scope.resultado = result.data.empleado;
-	            console.log($scope.resultado);
-		    }, function myError(response) {
-		        console.error(response);
-		        alert(response.data.header.mensajeFuncional);
-		    });
+	
 	
 		    $http({
               method: 'GET',
