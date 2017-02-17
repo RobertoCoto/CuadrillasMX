33ac93dd397cf4bec4f3bbcc4d12a3cfc2054d8b
Herson Sámano Avilés <Herson Smano Avils>
2017-02-17 01:16:15 -0600

    Version final de contrato sobre el menu

--- CuadrillasWEB/src/main/webapp/js/app.js
+++ CuadrillasWEB/src/main/webapp/js/app.js
@@ -1,5 +1,17 @@
 var app = angular.module('tatei', ['ui.bootstrap', 'ui.router']);
 var data;
+function removeByAttr(arr, attr, value) {
+  var i = arr.length;
+  while(i--){
+     if( arr[i]
+         && arr[i].hasOwnProperty(attr)
+         && (arguments.length > 2 && arr[i][attr] === value ) ){
+
+         arr.splice(i,1);
+     }
+  }
+  return arr;
+}
 
  app.config(function($routeProvider, $stateProvider) {
 	 $routeProvider
@@ -59,7 +71,7 @@ var data;
             .when('/50', {
                 templateUrl : 'templates/menu.html',
                 controller  : 'validaUsuario'
-            })            
+            })
             .when('/51', {
                 templateUrl : 'templates/menu.html',
                 controller  : 'validaUsuario'
@@ -67,13 +79,13 @@ var data;
             .when('/52', {
                 templateUrl : 'templates/menu.html',
                 controller  : 'validaUsuario'
-            })            
+            })
             .otherwise({redirectTo : '/login'});
-            
+
    		$stateProvider
             .state('10', {
             	url: '/10',
-                templateUrl : 'catalogo/index.html',
+                templateUrl : 'templates/catalogos.html',
                 controller  : 'catalogoctrl'
             })
             .state('12', {
@@ -82,7 +94,7 @@ var data;
             	controller  : 'cambioDatos'
             })
         	.state('13', {
-        		url: '/13', 
+        		url: '/13',
                 templateUrl : 'usuario/cambio_contra_gral.html',
                 controller  : 'adminDatos'
         	})
@@ -130,7 +142,7 @@ var data;
             	url: '/50',
                 templateUrl : 'reporte1 pendiente',
                 controller  : 'pendiente'
-            })            
+            })
             .state('51', {
             	url: '/51',
                 templateUrl : 'reporte2 pendiente',
@@ -140,11 +152,12 @@ var data;
             	url: '/52',
                 templateUrl : 'reporte3 pendiente',
                 controller  : 'pendiente'
-            });            
+            })
+            .state('otherwise', {redirectTo : '/login'});
     });
- 
+
     app.controller('validaUsuario',["$scope","$http", "$location", function ($scope,$http, $location) {
-		    $scope.validar = function() {		   
+		    $scope.validar = function() {
 		    	$http({
 		              method: 'GET',
 		              url: 'http://localhost:8080/CuadrillasWEB/ConsultaUsuarioLogin',
@@ -153,22 +166,22 @@ var data;
 				 		"password": document.getElementById("clave").value
 				         }
 				    }).then(function mySucces(response) {
-                        console.info(response);                        
-                        data = response;                            
+                        console.info(response);
+                        data = response;
                         $location.path('/menu');
 				        if (!$scope.$$phase && !$scope.$root.$$phase) {
 				            $scope.$apply();
 				            $('#contenedor').empty();
 				            console.log("Scope Apply Done !!");
-				          } 
+				          }
 				          else {
 				            setTimeout(function() {
 				            	$scope.$apply();
 				            	$('#contenedor').empty();
 				            }, 200);
-				          }                                                
+				          }
                         console.info("*******prueba*******");
-                        console.info(data);                        
+                        console.info(data);
                         document.getElementById("usuario").value="";
                         document.getElementById("clave").value="";
                   }, function myError(response) {
@@ -176,23 +189,23 @@ var data;
 				        alert(response.data.header.mensajeFuncional);
                         data = response;
                         console.info("*******prueba*******");
-                        console.info(data);				        
+                        console.info(data);
 				        //$location.path('/menu');
 				        //if (!$scope.$$phase && !$scope.$root.$$phase) {
 				        //    $scope.$apply();
 				        //    console.log("Scope Apply Done !!");
-				        //  } 
+				        //  }
 				        //  else {
 				        //    setTimeout(function() {
 				        //    	$scope.$apply();
 				        //    }, 200);
-				        //  }                        
+				        //  }
 				    });
-		    }		    
+		    }
 }]);
-    
 
-	app.controller('MainController', 
+
+	app.controller('MainController',
 		function ($scope) {
 	    	console.info(data.data.menu);
 	    	console.info("usuario");
@@ -200,7 +213,7 @@ var data;
 	    	$scope.usuario = data.data.usuario;
 	    	$scope.items = data.data.menu;
 	        $scope.default = $scope.items[0];
-	        
+
 	    });
 
     app.controller('ItemController', ['$scope', function (scope) {
@@ -212,7 +225,170 @@ var data;
 
             }]);
 
-    app.controller('vistaController', 
+    app.controller('vistaController',
         function ($scope) {
             document.getElementById("contenedor").style.marginLeft = "260px";
+        });
+
+    app.controller('catalogoctrl', function ($scope, $http) {
+
+          $('#alert').hide();
+          $('#success').hide();
+
+
+          var tipoCataloPadre = undefined;
+
+          //$scope.options1 = option1Options;
+          $('#msload').modal('show');
+          $('#alert').hide();
+          $('#success').hide();
+          $http({
+                  method: 'GET',
+                  url: 'http://localhost:8080/CuadrillasWEB/ConsultaTipoCatalogos',
+                  data: { }
+            }).then(function successfn(result) {
+              $scope.resultado = result.data.catalogo;
+                    //console.log(result);
+                     $('#msload').modal('hide');
+            }, function errorfn(response) {
+                console.error(response);
+                //alert(response.data.header.mensajeFuncional);
+                $('#msload').modal('hide');
+                $('#alert').show();
+                $('#msgerror').val(response.data.header.mensajeFuncional);
+                //$scope.resultado2.push(objecto);
+            });
+
+          $scope.options2 = []; // we'll get these later
+
+          $scope.saveOptions = function(objecto) {
+            $('#msload').modal('show');
+            $('#alert').hide();
+            $('#success').hide();
+            $http({
+                    method: 'GET',
+                    url: 'http://localhost:8080/CuadrillasWEB/RegistrarCatalogo',//?tipoCatalogo=VIALIDAD&codigo=SANL&descripcion=SAN%20LORENZO&usuario=SISTEMAS',
+                    params: {
+                      "tipoCatalogo": tipoCataloPadre,
+                      "codigo": document.getElementById("codigof").value.toUpperCase(),
+                      "usuario": data.data.usuario.usuario,
+                      "descripcion": document.getElementById("descripcionf").value.toUpperCase()
+                     }
+            }).then(function successfn(response) {
+                var fila = {};
+                fila.descripcion = document.getElementById("descripcionf").value.toUpperCase();
+                fila.codigo = document.getElementById("codigof").value.toUpperCase();
+                fila.tipo = 0;
+                document.getElementById("descripcionf").value = '';
+                document.getElementById("codigof").value = '';
+                $scope.resultado2.push(fila);
+                  //console.info(response);
+                  $('#msload').modal('hide');
+                  $('#success').show();
+                  $('#msgaviso').text(response.data.mensajeFuncional);
+            }, function errorfn(response) {
+                console.error(response);
+                //alert(response.data.mensajeFuncional);
+                $('#msload').modal('hide');
+                $('#alert').show();
+                $('#msgerror').text(response.data.mensajeFuncional);
+                //$scope.resultado2.push(objecto);
+            });
+
+
+          }
+
+          $scope.getOptions2 = function(aaa){
+            tipoCataloPadre = aaa;
+            //console.log(aaa);
+            $('#msload').modal('show');
+            $('#alert').hide();
+            $('#success').hide();
+            $http({
+                    method: 'GET',
+                    url: 'http://localhost:8080/CuadrillasWEB/ConsultaCatalogo',
+                    params: { "tipoCatalogo": aaa }
+                }).success(function (result) {
+                    $scope.resultado2 = result.catalogo;
+
+                    $scope.editingData = {};
+
+                     for (var i = 0, length = $scope.resultado2.length; i < length; i++) {
+                       $scope.editingData[$scope.resultado2[i].codigo] = false;
+                     }
+                     $('#msload').modal('hide');
+
+                     $scope.modify = function(catalogo){
+                         $scope.editingData[catalogo.codigo] = true;
+                     };
+
+                     $scope.cancelar = function(catalogo) {
+                       $scope.editingData[catalogo.codigo] = false;
+                     }
+
+                     $scope.update = function(catalogo){
+                        //console.warn(catalogo);
+                        //TODO ----- ACTUALIZAR HTTP
+                        $('#msload').modal('show');
+                        $('#alert').hide();
+                        $('#success').hide();
+                        $http({
+                               method: 'GET',
+                               url: 'http://localhost:8080/CuadrillasWEB/ActualizarCatalogo',//?tipoCatalogo=VIALIDAD&codigo=SANL&descripcion=SAN%20LORENZO&usuario=SISTEMAS
+                               params: {
+                                 "tipoCatalogo": tipoCataloPadre,
+                                 "descripcion": catalogo.descripcion,
+                                 "codigo": catalogo.codigo,
+                                 "usuario": data.data.usuario.usuario
+                                }
+                       }).then(function successfn(response) {
+                         $scope.editingData[catalogo.codigo] = false;
+                             //console.info(response);
+                             $('#msload').modal('hide');
+                             $('#success').show();
+                             $('#msgaviso').text(response.data.mensajeFuncional);
+                       }, function errorfn(response) {
+                           console.error(response);
+                           //alert(response.data.mensajeFuncional);
+                           $('#msload').modal('hide');
+                           $('#alert').show();
+                           $('#msgerror').text(response.data.mensajeFuncional);
+                           //$scope.resultado2.push(objecto);
+                       });
+
+                     };
+
+                     $scope.remove = function(catalogo) {
+                       //console.info(catalogo);
+                       $('#msload').modal('show');
+                       $('#alert').hide();
+                       $('#success').hide();
+                       $http({
+                               method: 'GET',
+                               url: 'http://localhost:8080/CuadrillasWEB/EliminarCatalogo',//?tipoCatalogo=VIALIDAD&codigo=SANLO&usuario=SISTEMAS',
+                               params: {
+                                 "tipoCatalogo": tipoCataloPadre,
+                                 "codigo": catalogo.codigo,
+                                 "usuario": data.data.usuario.usuario
+                                }
+                       }).then(function successfn(response) {
+                           removeByAttr($scope.resultado2, 'codigo',catalogo.codigo);
+                             //console.info(response);
+                             $('#msload').modal('hide');
+                             $('#success').show();
+                             $('#msgaviso').text(response.data.mensajeFuncional);
+                       }, function errorfn(response) {
+                           console.error(response);
+                           //alert(response.data.mensajeFuncional);
+                           $('#msload').modal('hide');
+                           $('#alert').show();
+                           $('#msgerror').text(response.data.mensajeFuncional);
+                           //$scope.resultado2.push(objecto);
+                       });
+
+
+                     }
+                    //console.log(result);
+            });
+          };
         });
