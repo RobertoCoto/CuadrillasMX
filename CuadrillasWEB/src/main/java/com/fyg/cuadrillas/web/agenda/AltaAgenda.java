9c5faaa2f21e23f394d1a3105991e7be64fdabe0
RobertoCoto <roberto.coto@fygsolutions.com>
2017-02-23 10:28:15 -0600

    se añade fechaActual

---
+++ CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/agenda/AltaAgenda.java
@@ -0,0 +1,80 @@
+package com.fyg.cuadrillas.web.agenda;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
+import com.fyg.cuadrillas.comun.LogHandler;
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class AltaAgenda
+ */
+public class AltaAgenda extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AltaAgenda() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+		Gson sg = new Gson();
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		try {
+			//leer json Array
+			JSONParser parser = new JSONParser();
+			Object JSONAltaAgenda = parser.parse(request.getParameter("JSONAltaAgenda"));
+			JSONObject jsonObject = (JSONObject) JSONAltaAgenda;
+			
+			/* descomentar para proxy FISA
+			System.setProperty("http.proxyHost", "169.169.4.85");
+	        System.setProperty("http.proxyPort", "8080");
+	        System.setProperty("https.proxyHost", "169.169.4.85");
+	        System.setProperty("https.proxyPort", "8080"); */
+			
+			if (respuesta.isEstatus()) {
+				response.setStatus(HttpServletResponse.SC_OK);
+			} else {
+				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			}
+			//convierte  a formato Json
+			out.println(sg.toJson(respuesta));
+			out.flush();
+		} catch (Exception e) {
+			LogHandler.error("", this.getClass(), "Error servlet", e);
+			respuesta.setMensajeFuncional("Error: " + e.getMessage());
+			respuesta.setEstatus(false);
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			out.println(sg.toJson(respuesta));
+			out.flush();
+		}
+	}
+
+}
