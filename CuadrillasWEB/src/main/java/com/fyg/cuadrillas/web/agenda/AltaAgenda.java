4f6d3ecde6ce32bb5af7a106245ae19622adf119
RobertoCoto <roberto.coto@fygsolutions.com>
2017-02-23 14:10:30 -0600

    servlet altaAgenda

--- CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/agenda/AltaAgenda.java
+++ CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/agenda/AltaAgenda.java
@@ -16,7 +16,9 @@ import org.json.simple.parser.JSONParser;
 
 import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
 import com.fyg.cuadrillas.comun.LogHandler;
+import com.fyg.cuadrillas.dto.CoordenadaDTO;
 import com.fyg.cuadrillas.dto.agenda.AgendaDTO;
+import com.fyg.cuadrillas.dto.agenda.AgendaDetalleDTO;
 import com.fyg.cuadrillas.negocio.AgendaNegocio;
 import com.google.gson.Gson;
 
@@ -75,6 +77,10 @@ public class AltaAgenda extends HttpServlet {
 			Integer noTrabajadores = (Integer) jsonObject.get("noTrabajadores");
 			Integer noSemana = (Integer) jsonObject.get("noSemana"); 
 			String usuario = (String) jsonObject.get("codigoContrato");
+			JSONArray listaAgendaDetalle = (JSONArray) jsonObject.get("agendaDetalle");
+			
+			//Se crea lista de agendaDetalle
+			List<AgendaDetalleDTO> agendaDetalle = new ArrayList<AgendaDetalleDTO>();
 			
 			//se pasan a nuestros objetos 
 			agenda.setIdContrato(idContrato);
@@ -84,6 +90,74 @@ public class AltaAgenda extends HttpServlet {
 			agenda.setNoSemana(noSemana);
 			agenda.setNoTrabajadores(noTrabajadores);
 			agenda.setUsuario(usuario);
+			
+			//se obtiene los detalles de la agenda
+			
+			for(int i = 0; i < listaAgendaDetalle.size(); i++)
+			{
+				AgendaDetalleDTO  detalle = new AgendaDetalleDTO();
+				JSONObject detalleAgenda = (JSONObject) listaAgendaDetalle.get(i);
+				String fecha = (String) detalleAgenda.get("fecha");
+				Integer avanceEsperado = (Integer) detalleAgenda.get("avanceEsperado");
+				String observaciones = (String) detalleAgenda.get("observaciones");
+				String usuarioAlta = (String) detalleAgenda.get("usuarioAgenda");
+				
+				JSONArray agendaActividades = (JSONArray) detalleAgenda.get("Actividades");
+				JSONArray agendaMateriales = (JSONArray) detalleAgenda.get("Materiales");
+				JSONArray agendaCoordenadas = (JSONArray)  detalleAgenda.get("coordenadas");
+				
+				List<String> actividades = new ArrayList<String>();
+				List<String> materiales  = new ArrayList<String>();
+				List<CoordenadaDTO> coordenadas = new ArrayList<CoordenadaDTO>();
+				
+				for (int k = 0; k < agendaActividades.size(); k++)
+				{
+					
+					JSONObject agendaActividad = (JSONObject) agendaActividades.get(k);
+					String codigoActividad = (String) agendaActividad.get("codigoActividad");
+					String usuarioActividad = (String) agendaActividad.get("usuarioActividad");
+					actividades.add(codigoActividad);
+					actividades.add(usuarioActividad);
+					
+				}
+				
+				for (int j=0; j< agendaMateriales.size(); j++) {
+					JSONObject material = (JSONObject) agendaMateriales.get(j);
+					String codigoMaterial = (String) material.get("codigoMaterial");
+					String usuarioMaterial = (String) material.get("usuarioMaterial");
+					
+					materiales.add(usuarioMaterial);
+					materiales.add(codigoMaterial);
+				}
+				
+				for (int l=0; l< agendaCoordenadas.size(); l++) {
+					CoordenadaDTO codigo = new CoordenadaDTO();
+					
+					JSONObject coorde = (JSONObject) agendaCoordenadas.get(l);
+					Integer orden = (Integer) coorde.get("orden");
+					String direccion = (String)  coorde.get("direccion");
+					Float latitud = (Float) coorde.get("latitud");
+					Float longitud = (Float) coorde.get("longitud");
+					codigo.setDireccion(direccion);
+					codigo.setLatitud(latitud);
+					codigo.setLongitud(longitud);
+					codigo.setOrden(orden);
+				    coordenadas.add(codigo);
+					
+				}
+				
+				
+				 detalle.setActividades(actividades);
+				 detalle.setMateriales(materiales);
+				 detalle.setCoordenadas(coordenadas);
+				 detalle.setAvanceEsperado(avanceEsperado);
+				 detalle.setFecha(fecha);
+				 detalle.setObservaciones(observaciones);
+				 detalle.setUsuarioAlta(usuarioAlta);
+				 agendaDetalle.add(detalle);
+				
+			}
+			agenda.setDiasAgenda(agendaDetalle);
 			respuesta = negocio.altaAgenda(agenda);
 			if (respuesta.isEstatus()) {
 				response.setStatus(HttpServletResponse.SC_OK);
