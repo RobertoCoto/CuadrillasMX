63b46af965646480e6e6297579d82b89ff8ec7a6
RobertoCoto <roberto.coto@fygsolutions.com>
2017-02-24 15:36:42 -0600

    cambios servlet

--- CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/agenda/ConsultaAgenda.java
+++ CuadrillasWEB/src/main/java/com/fyg/cuadrillas/web/agenda/ConsultaAgenda.java
@@ -1,10 +1,17 @@
 package com.fyg.cuadrillas.web.agenda;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fyg.cuadrillas.comun.LogHandler;
+import com.fyg.cuadrillas.dto.agenda.AgendaRespuesta;
+import com.fyg.cuadrillas.negocio.AgendaNegocio;
+import com.google.gson.Gson;
 
 /**
  * Servlet implementation class ConsultaAgenda
@@ -24,14 +31,38 @@ public class ConsultaAgenda extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		this.doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		AgendaRespuesta respuesta = new AgendaRespuesta();
+		Gson sg = new Gson();
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		try {
+			//crea objeto de negocio
+			final AgendaNegocio  negocio = new AgendaNegocio();
+			respuesta = negocio.consultaAgenda();
+			if (respuesta.getHeader().isEstatus()) {
+				response.setStatus(HttpServletResponse.SC_OK);
+			} else {
+				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			}
+			//convierte  a formato Json
+			out.println(sg.toJson(respuesta));
+			out.flush();
+		} catch (Exception e) {
+			LogHandler.error("", this.getClass(), "Error servlet", e);
+			respuesta.getHeader().setMensajeFuncional("Error: " + e.getMessage());
+			respuesta.getHeader().setEstatus(false);
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			out.println(sg.toJson(respuesta));
+			out.flush();
+		}
 	}
 
 }
