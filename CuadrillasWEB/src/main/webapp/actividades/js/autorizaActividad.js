7b24aee8ee0e499f8ed5fc8485cf4390dde074d9
RobertoCoto <roberto.coto@fygsolutions.com>
2017-03-22 12:28:20 -0600

    carga del mapa

--- CuadrillasWEB/src/main/webapp/actividades/js/autorizaActividad.js
+++ CuadrillasWEB/src/main/webapp/actividades/js/autorizaActividad.js
@@ -1,4 +1,28 @@
 var app = angular.module('tatei', []);
 app.controller('autorizaActividad', function ($scope, $http, $window) {
 	
+		var map;
+        var medida;
+       $scope.cargarMapa =  function initMap() {
+            medida = {
+                mvcLine: new google.maps.MVCArray(),
+                mvcPolygon: new google.maps.MVCArray(),
+                mvcMarkers: new google.maps.MVCArray(),
+                line: null,
+                polygon: null
+            };
+            var mx1 = {lat: 19.34751544463381, lng: -98.98272888210454};
+            map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 7,
+              center: mx1
+            });
+            google.maps.event.addListener(map, 'click', function(event) {
+              //console.log(event);
+                setMarcador(event.latLng);
+            });
+          }
+
+            var markers = [];
+      
+      $scope.cargarMapa();
 	});
