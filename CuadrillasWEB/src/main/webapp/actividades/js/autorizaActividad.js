c23d00f589a22abc7b2d0082bc48f2eef403b8b2
RobertoCoto <roberto.coto@fygsolutions.com>
2017-03-22 18:25:38 -0600

    cambio act

--- CuadrillasWEB/src/main/webapp/actividades/js/autorizaActividad.js
+++ CuadrillasWEB/src/main/webapp/actividades/js/autorizaActividad.js
@@ -2,7 +2,9 @@ var app = angular.module('tatei', []);
 app.controller('autorizaActividad', function ($scope, $http, $window) {
 	
 		var map;
+		var map2;
         var medida;
+        var medida2;
        $scope.cargarMapa =  function initMap() {
             medida = {
                 mvcLine: new google.maps.MVCArray(),
@@ -20,9 +22,29 @@ app.controller('autorizaActividad', function ($scope, $http, $window) {
               //console.log(event);
                 setMarcador(event.latLng);
             });
-          }
+          };
+          
+          $scope.cargarMapa2 =  function initMap2() {
+            medida2 = {
+                mvcLine: new google.maps.MVCArray(),
+                mvcPolygon: new google.maps.MVCArray(),
+                mvcMarkers: new google.maps.MVCArray(),
+                line: null,
+                polygon: null
+            };
+            var mx1 = {lat: 19.34751544463381, lng: -98.98272888210454};
+            map2 = new google.maps.Map(document.getElementById('map2'), {
+              zoom: 7,
+              center: mx1
+            });
+            google.maps.event.addListener(map, 'click', function(event) {
+              //console.log(event);
+                setMarcador(event.latLng);
+            });
+          };
 
             var markers = [];
       
       $scope.cargarMapa();
+      $scope.cargarMapa2();
 	});
