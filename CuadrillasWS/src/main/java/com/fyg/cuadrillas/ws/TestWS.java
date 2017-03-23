8156378bd9389acc482b18cc11348c0fdccf5087
RobertoCoto <roberto.coto@fygsolutions.com>
2017-03-23 17:23:32 -0600

    WS CUADRILLAS V1

---
+++ CuadrillasWS/src/main/java/com/fyg/cuadrillas/ws/TestWS.java
@@ -0,0 +1,36 @@
+package com.fyg.cuadrillas.ws;
+
+public class TestWS {
+	/**
+	 * Empleado
+	 */
+	private String empleado;
+	/**
+	 * msg
+	 */
+	private String msg;
+	/**
+	 * @return the empleado
+	 */
+	public String getEmpleado() {
+		return empleado;
+	}
+	/**
+	 * @param empleado the empleado to set
+	 */
+	public void setEmpleado(String empleado) {
+		this.empleado = empleado;
+	}
+	/**
+	 * @return the msg
+	 */
+	public String getMsg() {
+		return msg;
+	}
+	/**
+	 * @param msg the msg to set
+	 */
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
