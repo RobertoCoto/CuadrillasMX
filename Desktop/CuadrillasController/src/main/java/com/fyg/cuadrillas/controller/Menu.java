ec82a2fe7ba85727fea93ba16b811d33507c6ac4
RobertoCoto <roberto.coto@fygsolutions.com>
2017-03-25 08:30:05 -0600

    Cambio menu

--- Desktop/CuadrillasController/src/main/java/com/fyg/cuadrillas/controller/Menu.java
+++ Desktop/CuadrillasController/src/main/java/com/fyg/cuadrillas/controller/Menu.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JMenuBar;
 
 public class Menu extends JFrame {
 
@@ -31,13 +33,14 @@ public class Menu extends JFrame {
 	 * Create the frame.
 	 */
 	public Menu() {
+		setResizable(false);
 		setTitle("Menu Principal - TATEI");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 750, 505);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
 	}
 
 }
