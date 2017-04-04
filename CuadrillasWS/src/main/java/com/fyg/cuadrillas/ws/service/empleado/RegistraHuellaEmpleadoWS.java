5a2e2a1be559038ead55c76a67e332399203901e
RobertoCoto <roberto.coto@fygsolutions.com>
2017-04-04 11:45:57 -0500

    limpieza checstyle

--- CuadrillasWS/src/main/java/com/fyg/cuadrillas/ws/service/empleado/RegistraHuellaEmpleadoWS.java
+++ CuadrillasWS/src/main/java/com/fyg/cuadrillas/ws/service/empleado/RegistraHuellaEmpleadoWS.java
@@ -11,16 +11,23 @@ import com.fyg.cuadrillas.comun.EncabezadoRespuesta;
 import com.fyg.cuadrillas.dto.empleado.EmpleadoHuellaDTO;
 import com.fyg.cuadrillas.negocio.EmpleadoNegocio;
 import com.google.gson.Gson;
-import com.mysql.jdbc.Blob;
 
 @Path("/registraHuella")
 public class RegistraHuellaEmpleadoWS {
+	/**
+	 * Metodo para registrar la huella del empleado
+	 * @param idEmpleado id del empleado
+	 * @param mano codigo mano
+	 * @param dedo codigo dedo
+	 * @param ruta ruta de la imagen
+	 * @return regresa respuesta
+	 */
 	@GET
 	@Path("/huella")
 	@Produces({MediaType.APPLICATION_JSON})
 	public Response regHuella(@QueryParam("idEmpleado")Integer idEmpleado,
-			@QueryParam("codigoMano")String mano,@QueryParam("codigoDedo")String dedo
-			,@QueryParam("ruta") String ruta) {
+			@QueryParam("codigoMano")String mano, @QueryParam("codigoDedo")String dedo
+			, @QueryParam("ruta") String ruta) {
 		EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
 		Gson sg = new Gson();
 		try {
@@ -31,7 +38,6 @@ public class RegistraHuellaEmpleadoWS {
 			huella.setCodigoDedo(dedo);
 			huella.setHuella(ruta);
 			respuesta = negocio.registrarHuella(huella);
-			
 		} catch (Exception x) {
 			String result = sg.toJson(respuesta);
 			return Response.serverError().entity(result).build();
