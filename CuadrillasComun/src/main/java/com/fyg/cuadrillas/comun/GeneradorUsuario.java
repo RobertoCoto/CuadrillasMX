2fd31248aa1fbd784895290affb719824eb4b356
RobertoCoto <roberto.coto@fygsolutions.com>
2017-04-17 16:00:44 -0500

    clase generador usuario

---
+++ CuadrillasComun/src/main/java/com/fyg/cuadrillas/comun/GeneradorUsuario.java
@@ -0,0 +1,16 @@
+package com.fyg.cuadrillas.comun;
+
+public class GeneradorUsuario {
+/**
+ * Metodo para generar usuario
+ * @param nombre recibe el nombre
+ * @param apellidoPaterno recibe un apellido
+ * @return retorna usuario
+ */
+	public String generaUsuario(String nombre, String apellidoPaterno) {
+		String usuario= "";
+		char pr = nombre.charAt(0);
+		usuario = pr+""+apellidoPaterno;
+		return usuario.toLowerCase();
+	}
+}
